09b7ae073335458a4cdd7657db315f7da28a9867
ChaYoung <yousbe@gmail.com>
2013-10-05 13:16:44 +0900

    Reduce code (Runtime Error)

--- problem/1002/main.rb
+++ problem/1002/main.rb
@@ -1,1 +1,1 @@
-gets.to_i.times{x,y,r,p,q,l=gets.split.map(&:to_i);d=Math.hypot(p-x,q-y);m=r+l;n=(r-l).abs;p(x==p&&y==q&&r==l ?-1:m<d||n>d ?0:m==d||n==d ?1:2)}
+gets.to_i.times{x,y,r,p,q,l=gets.split.map(&:to_i);d=Math.hypot(p-x,q-y);m=r+l;n=(r-l).abs;p(d==0&&r==l ?-1:d<n|m<d ?0:n<d|d<m ?2:1)}
