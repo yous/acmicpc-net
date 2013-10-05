ed74819f17092d0c6daf47e1cb5926d3f66d9215
ChaYoung <yousbe@gmail.com>
2013-10-05 13:15:54 +0900

    Add missed conditions

--- problem/1002/main.rb
+++ problem/1002/main.rb
@@ -1,1 +1,1 @@
-gets.to_i.times{x,y,r,p,q,l=gets.split.map(&:to_i);d=Math.hypot(p-x,q-y);p(x==p&&y==q&&r==l ?-1:r>d||l>d ?0:(r+l<=>d)+1)}
+gets.to_i.times{x,y,r,p,q,l=gets.split.map(&:to_i);d=Math.hypot(p-x,q-y);m=r+l;n=(r-l).abs;p(x==p&&y==q&&r==l ?-1:m<d||n>d ?0:m==d||n==d ?1:2)}
