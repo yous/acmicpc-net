38d9c927c46d57ce198459a1bed221bac9325cb0
ChaYoung <yousbe@gmail.com>
2013-10-05 13:19:23 +0900

    Change '|' operators to '||' to fix error (Wrong)

--- problem/1002/main.rb
+++ problem/1002/main.rb
@@ -1,1 +1,1 @@
-gets.to_i.times{x,y,r,p,q,l=gets.split.map(&:to_i);d=Math.hypot(p-x,q-y);m=r+l;n=(r-l).abs;p(d==0&&r==l ?-1:d<n|m<d ?0:n<d|d<m ?2:1) }
+gets.to_i.times{x,y,r,p,q,l=gets.split.map(&:to_i);d=Math.hypot(p-x,q-y);m=r+l;n=(r-l).abs;p(d==0&&r==l ?-1:d<n||m<d ?0:n<d||d<m ?2:1)}
