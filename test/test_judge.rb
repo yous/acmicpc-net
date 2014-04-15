a170d56741c8cc9dc902361ec9dc27d21faf88bf
ChaYoung You <yousbe@gmail.com>
2014-04-16 02:15:50 +0900

    Add short.rb unit test

--- test/test_judge.rb
+++ test/test_judge.rb
@@ -6,5 +6,8 @@ class TestJudge < Test::Unit::TestCase
   def test_judge
     file_path = "problem/#{$argv[-1]}"
     assert_equal %x[ruby #{file_path}/main.rb < #{file_path}/input], File.read("#{file_path}/output")
+    if File.exists? "#{file_path}/short.rb"
+      assert_equal %x[ruby #{file_path}/short.rb < #{file_path}/input], File.read("#{file_path}/output")
+    end
   end
 end
