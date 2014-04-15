7582992ac0f2c13e88b88594a36f4f1a90b10add
ChaYoung You <yousbe@gmail.com>
2014-04-16 01:54:41 +0900

    Replace tab with spaces

--- test/test_judge.rb
+++ test/test_judge.rb
@@ -3,8 +3,8 @@ require 'test/unit'
 $argv = ARGV.dup
 
 class TestJudge < Test::Unit::TestCase
-	def test_judge
-		file_path = "problem/#{$argv[-1]}"
-		assert_equal %x[ruby #{file_path}/main.rb < #{file_path}/input], File.read("#{file_path}/output")
-	end
+  def test_judge
+    file_path = "problem/#{$argv[-1]}"
+    assert_equal %x[ruby #{file_path}/main.rb < #{file_path}/input], File.read("#{file_path}/output")
+  end
 end
