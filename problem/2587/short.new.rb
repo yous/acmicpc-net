370407a9900f086aadf5cc00b9afb4d40614d511
ChaYoung You <yousbe@gmail.com>
2014-08-04 05:44:37 +0900

    Solve 2587

---
+++ problem/2587/short.new.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+n=[];5.times{n<<gets.to_i};p n.reduce(:+)/n.size,n.sort[2]
+
+# nums = []
+# 5.times do
+#   nums << gets.to_i
+# end
+# p nums.reduce(:+) / nums.size
+# p nums.sort[2]
