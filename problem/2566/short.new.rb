cfa3f96f3840b64aa4a054dc20c870a91350d023
ChaYoung You <yousbe@gmail.com>
2014-08-04 04:48:21 +0900

    Solve 2566

---
+++ problem/2566/short.new.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+a=[];9.times{a+=gets.split.map(&:to_i)};p m=a.max;i=a.index m;puts"#{i/9+1} #{i%9+1}"
+
+# matrix = []
+# 9.times do
+#   matrix += gets.split.map(&:to_i)
+# end
+# max, index = matrix.each_with_index.max
+# p max
+# puts "#{index / 9 + 1} #{index % 9 + 1}"
