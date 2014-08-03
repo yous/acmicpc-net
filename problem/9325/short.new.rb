7a88fae9158dad5e61841ce6047b6df741e2d0f6
ChaYoung You <yousbe@gmail.com>
2014-08-03 23:09:29 +0900

    Solve 9325

---
+++ problem/9325/short.new.rb
@@ -0,0 +1,9 @@
+gets.to_i.times{s=gets.to_i;gets.to_i.times{s+=gets.split.map(&:to_i).reduce(:*)};puts s}
+
+# gets.to_i.times do
+#   result = gets.to_i
+#   gets.to_i.times do
+#     result += gets.split.map(&:to_i).reduce(:*)
+#   end
+#   puts result
+# end
