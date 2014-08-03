54852503162cb88ab90e10d4c954e4e889935fe9
ChaYoung You <yousbe@gmail.com>
2014-08-03 18:11:41 +0900

    Test all problems if there are no arguments

--- test/test_judge.rb
+++ test/test_judge.rb
@@ -2,10 +2,17 @@ require 'test/unit'
 
 class TestJudge < Test::Unit::TestCase
   def test_judge
-    file_path = "problem/#{ENV["PROBLEM"]}"
-    assert_equal %x[ruby #{file_path}/main.rb < #{file_path}/input], File.read("#{file_path}/output")
-    if File.exists? "#{file_path}/short.rb"
-      assert_equal %x[ruby #{file_path}/short.rb < #{file_path}/input], File.read("#{file_path}/output")
+    problems = if ENV["PROBLEM"]
+                 ENV["PROBLEM"].split(",")
+               else
+                 Dir["problem/*"].map { |v| v.sub(/^problem\//, "") }
+               end
+    problems.each do |problem|
+      file_path = "problem/#{problem}"
+      assert_equal %x[ruby #{file_path}/main.rb < #{file_path}/input], File.read("#{file_path}/output")
+      if File.exist? "#{file_path}/short.rb"
+        assert_equal %x[ruby #{file_path}/short.rb < #{file_path}/input], File.read("#{file_path}/output")
+      end
     end
   end
 end
