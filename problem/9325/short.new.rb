e1935b59d9f9de7a3b22134274119a00ed7feb37
ChaYoung You <yousbe@gmail.com>
2014-08-04 05:53:40 +0900

    Reduce 9325

--- problem/9325/short.new.rb
+++ problem/9325/short.new.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
-gets.to_i.times{s=gets.to_i;gets.to_i.times{s+=gets.split.map(&:to_i).reduce(:*)};puts s}
+gets.to_i.times{s=gets.to_i;gets.to_i.times{s+=gets.split.map(&:to_i).reduce:*};p s}
 
 # gets.to_i.times do
 #   result = gets.to_i
 #   gets.to_i.times do
 #     result += gets.split.map(&:to_i).reduce(:*)
 #   end
-#   puts result
+#   p result
 # end
