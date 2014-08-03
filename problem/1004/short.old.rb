53e899bc5ce80e76955b38e76856e53ddfd7a743
ChaYoung You <yousbe@gmail.com>
2014-08-04 07:44:34 +0900

    Reduce 1004 using proc

--- problem/1004/short.old.rb
+++ problem/1004/short.old.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-gets.to_i.times{p,q,r,s=gets.split.map(&:to_i);puts((1..gets.to_i).map{x,y,n=gets.split.map(&:to_i);(Math.hypot(p-x,q-y)-n)*(n-Math.hypot(r-x,s-y))>0}.count true)}
+g=proc{gets.split.map &:to_i};g[][0].times{p,q,r,s=g[];puts((1..g[][0]).map{x,y,n=g[];(Math.hypot(p-x,q-y)-n)*(n-Math.hypot(r-x,s-y))>0}.count true)}
 
 # gets.to_i.times do
 #   p, q, r, s = gets.split.map(&:to_i)
