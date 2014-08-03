199dae3923e5839917fd1d084a07858b5f8e6355
ChaYoung You <yousbe@gmail.com>
2014-08-03 23:33:28 +0900

    Simplify 1100

--- problem/1100/short.new.rb
+++ problem/1100/short.new.rb
@@ -1,10 +1,8 @@
-r=i=0;8.times{r+=(i..(l=gets).length-2).step(2).count{|v|l[v]=="F"};i=i==0?1:0};p r
+r=0;8.times{|i|r+=(i%2..(l=gets).length-2).step(2).count{|v|l[v]=="F"}};p r
 
 # result = 0
-# offset = 0
-# 8.times do
+# 8.times do |line_idx|
 #   line = gets.chomp
-#   result += (offset...line.length).step(2).count { |i| line[i] == "F" }
-#   offset = offset.zero? ? 1 : 0
+#   result += (line_idx % 2...line.length).step(2).count { |i| line[i] == "F" }
 # end
 # p result
