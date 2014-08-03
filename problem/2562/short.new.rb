c58ec5d00f7d7fa50922995292f441c372eb000f
ChaYoung You <yousbe@gmail.com>
2014-08-04 05:47:10 +0900

    Reduce 2562

--- problem/2562/short.new.rb
+++ problem/2562/short.new.rb
@@ -1,10 +1,9 @@
 # encoding: utf-8
-n=[];9.times{n<<gets.to_i};p m=n.max;p n.index(m)+1
+n=[];9.times{n<<gets.to_i};p m=n.max,n.index(m)+1
 
 # numbers = []
 # 9.times do
 #   numbers << gets.to_i
 # end
 # max, index = numbers.each_with_index.max
-# p max
-# p index + 1
+# p max, index + 1
