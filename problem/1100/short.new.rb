5a6c0f1ff76a64e88a83b64a55ad82206b3f1dfa
ChaYoung You <yousbe@gmail.com>
2014-08-03 23:43:51 +0900

    Reduce code by replacing length with size

--- problem/1100/short.new.rb
+++ problem/1100/short.new.rb
@@ -1,8 +1,8 @@
-r=0;8.times{|i|r+=(i%2..(l=gets).length-2).step(2).count{|v|l[v]=="F"}};p r
+r=0;8.times{|i|r+=(i%2..(l=gets).size-2).step(2).count{|v|l[v]=="F"}};p r
 
 # result = 0
 # 8.times do |line_idx|
 #   line = gets.chomp
-#   result += (line_idx % 2...line.length).step(2).count { |i| line[i] == "F" }
+#   result += (line_idx % 2...line.size).step(2).count { |i| line[i] == "F" }
 # end
 # p result
