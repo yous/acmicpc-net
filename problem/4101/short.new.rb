29b82be54ffbc6fad8ae7433497a6a9ac6cc2a5a
ChaYoung You <yousbe@gmail.com>
2014-08-04 03:37:02 +0900

    Reduce 4101

--- problem/4101/short.new.rb
+++ problem/4101/short.new.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-g=->(){gets.split.map(&:to_i)};m,n=g[];until m==0&&n==0;puts m>n ?"Yes":"No";m,n=g[]end
+g=->(){gets.split.map &:to_i};m,n=g[];while m|n>0;puts m>n ?"Yes":"No";m,n=g[]end
 
 # m, n = gets.split.map(&:to_i)
 # until m == 0 && n == 0
