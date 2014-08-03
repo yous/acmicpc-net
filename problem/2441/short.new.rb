03f7377a45fbaeabbed896f17563767c2bfb9465
ChaYoung You <yousbe@gmail.com>
2014-08-04 02:06:44 +0900

    Reduce 2441

--- problem/2441/short.new.rb
+++ problem/2441/short.new.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
-(n=gets.to_i).downto(1){|i|puts" "*(n-i)+"*"*i}
+(n=gets.to_i).times{|i|puts" "*i+"*"*(n-i)}
 
 # n = gets.to_i
-# n.downto(1) do |i|
-#   puts format("%#{n}s", "*" * i)
+# n.times do |i|
+#   puts format("%#{n}s", "*" * (n - i))
 # end
