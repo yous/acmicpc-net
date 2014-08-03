76aa6b7272154003d12fb9487652a927660492f0
ChaYoung You <yousbe@gmail.com>
2014-08-04 07:18:40 +0900

    Reduce 1026

--- problem/1026/short.new.rb
+++ problem/1026/short.new.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
-g=->(){gets.split.map &:to_i};gets.to_i;a=g[];b=g[];p a.sort.reverse.zip(b.sort).map{|v|v.reduce:*}.reduce:+
+g=->(){gets.split.map(&:to_i).sort};g[];a=g[].reverse;b=g[];p a.zip(b).map{|v|v.reduce:*}.reduce:+
 
 # gets.to_i
-# a = gets.split.map(&:to_i)
-# b = gets.split.map(&:to_i)
-# p a.sort.reverse.zip(b.sort).map { |v| v.reduce(:*) }.reduce(:+)
+# a = gets.split.map(&:to_i).sort.reverse
+# b = gets.split.map(&:to_i).sort
+# p a.zip(b).map { |v| v.reduce(:*) }.reduce(:+)
