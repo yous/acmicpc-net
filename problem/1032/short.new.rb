49812b255ebe678d1b9520d3b77472e26aa0780e
ChaYoung You <yousbe@gmail.com>
2014-08-04 07:34:09 +0900

    Solve 1032

---
+++ problem/1032/short.new.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+f=[];gets.to_i.times{f<<gets.chomp.chars.to_a};puts f.transpose.map{|c|c.uniq.size==1?c[0]:"?"}.join
+
+# files = []
+# gets.to_i.times do
+#   files << gets.chomp.chars.to_a
+# end
+# result = files.transpose.map do |chars|
+#   if chars.uniq.size == 1
+#     chars[0]
+#   else
+#     "?"
+#   end
+# end
+# puts result.join
