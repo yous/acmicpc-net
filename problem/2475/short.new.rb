f9d8cd3616c26a1098997bf506be3207f0bba388
ChaYoung You <yousbe@gmail.com>
2014-08-04 03:09:32 +0900

    Reduce 2475

--- problem/2475/short.new.rb
+++ problem/2475/short.new.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
-p gets.split.reduce(0){|a,e|e=e.to_i;a+e*e}%10
+p gets.split.reduce(0){|a,e|a+e.to_i**2}%10
 
 # ids = gets.split.map(&:to_i)
 # result = ids.reduce(0) do |sum, value|
-#   sum + value * value
+#   sum + value ** 2
 # end
 # p result % 10
