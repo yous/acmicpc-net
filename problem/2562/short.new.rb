d3e6fc71ce597ef376b5fa752fda530d49e1af00
ChaYoung You <yousbe@gmail.com>
2014-08-04 02:22:14 +0900

    Solve 2562

---
+++ problem/2562/short.new.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+n=[];9.times{n<<gets.to_i};p m=n.max;p n.index(m)+1
+
+# numbers = []
+# 9.times do
+#   numbers << gets.to_i
+# end
+# max, index = numbers.each_with_index.max
+# p max
+# p index + 1
