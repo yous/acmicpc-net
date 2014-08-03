2ff0d91b16805400413729d310514d04c5e35cc8
ChaYoung You <yousbe@gmail.com>
2014-08-04 05:59:25 +0900

    Solve 2588

---
+++ problem/2588/short.new.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+n=gets.to_i;m=gets.to_i;p m%10*n,m/10%10*n,m/100*n,n*m
+
+# num1 = gets.to_i
+# num2 = gets.to_i
+# p num2 % 10 * num1,
+#   num2 / 10 % 10 * num1,
+#   num2 / 100 * num1,
+#   num1 * num2
