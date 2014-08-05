23fc0e3da07f3121f66298a76303e5e882e8a138
ChaYoung You <yousbe@gmail.com>
2014-08-05 17:28:44 +0900

    Rewrite 1016 (Timeout)

--- problem/1016/short.new.rb
+++ problem/1016/short.new.rb
@@ -1,7 +1,15 @@
 # encoding: utf-8
-require"prime";n,m=gets.split.map &:to_i;p (n..m).count{|v|v.prime_division.all?{|_,e|e==1}}
+require"prime";n,m=gets.split.map &:to_i;p (n..m).count{|c|Prime.each{|p|break 1if c==1;(c/=p;break 0if c%p==0)if c%p==0}>0}
 
 # require "prime"
 # min, max = gets.split.map(&:to_i)
-# count = (min..max).count { |num| num.prime_division.all? { |_p, n| n == 1 } }
+# count = (min..max).count do |num|
+#   Prime.each do |p|
+#     break true if num == 1
+#     if num % p == 0
+#       num /= p
+#       break false if num % p == 0
+#     end
+#   end
+# end
 # p count
