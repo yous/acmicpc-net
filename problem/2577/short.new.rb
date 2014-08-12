0ed257ff0785fc599d44f222d3428f08ffbc558e
ChaYoung You <yousbe@gmail.com>
2014-08-13 05:08:09 +0900

    Reduce 2577 (65)

--- problem/2577/short.new.rb
+++ problem/2577/short.new.rb
@@ -1,14 +1,11 @@
 # encoding: utf-8
-n=1;3.times{n*=gets.to_i};r=Hash.new(0);n.to_s.each_char{|c|r[c.to_i]+=1};10.times{|i|p r[i]}
+n=1;$<.map{|x|n*=x.to_i};10.times{|i|p n.to_s.chars.count i.to_s}
 
 # nums = []
 # 3.times do
 #   nums << gets.to_i
 # end
-# result = Hash.new(0)
-# nums.reduce(:*).to_s.each_char do |ch|
-#   result[ch.to_i] += 1
-# end
+# num_chars = nums.reduce(:*).to_s.chars
 # 10.times do |num|
-#   p result[num]
+#   p num_chars.count(num.to_s)
 # end
