245ac9deb683699e8187d2456e5024d0c3b1b35b
ChaYoung You <yousbe@gmail.com>
2014-08-13 06:41:29 +0900

    Reduce 2592 (69)

--- problem/2592/short.new.rb
+++ problem/2592/short.new.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-n=[];10.times{n<<gets.to_i};p n.reduce(:+)/n.size,n.map{|v|[n.count(v),v]}.max[1]
+n=$<.map &:to_i;p n.reduce(:+)/n.size,n.map{|v|[n.count(v),v]}.max[1]
 
 # nums = []
 # 10.times do
