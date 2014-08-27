e6ea4bf4761011226f5483e0d5ab439b0a27f0b1
ChaYoung You <yousbe@gmail.com>
2014-08-28 05:59:09 +0900

    Reduce 1026 (96)

--- problem/1026/short.new.rb
+++ problem/1026/short.new.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-g=->(){gets.split.map(&:to_i).sort};g[];a=g[].reverse;b=g[];p a.zip(b).map{|v|v.reduce:*}.reduce:+
+g=->{gets.split.map(&:to_i).sort};g[];a=g[].reverse;b=g[];p a.zip(b).map{|v|v.reduce:*}.reduce:+
 
 # gets.to_i
 # a = gets.split.map(&:to_i).sort.reverse
