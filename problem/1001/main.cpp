dc3d96abe90d0b88a093d70bed213efdcd579199
Chayoung You <yousbe@gmail.com>
2021-05-08 21:57:56 +0900

    C++11: solve 1001

---
+++ problem/1001/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a - b << "\n";
+    return 0;
+}
