4f66f7ffff893ddb9ffbc89ae17732198b817aa2
Chayoung You <yousbe@gmail.com>
2021-05-08 21:58:10 +0900

    C++11: solve 1008

---
+++ problem/1008/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    cout.precision(10);
+    cout << a / b << "\n";
+    return 0;
+}
