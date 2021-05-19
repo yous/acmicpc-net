b593633133ca544ab2ce1187170791dfcd0cf947
Chayoung You <yousbe@gmail.com>
2021-05-20 02:54:22 +0900

    10171: solve in C++11

---
+++ problem/10171/main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const string cat =
+"\\    /\\\n"
+" )  ( ')\n"
+"(  /  )\n"
+" \\(__)|\n";
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << cat;
+    return 0;
+}
