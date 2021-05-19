60303d81ea778f02f33e882e45944adaa2c5abe5
Chayoung You <yousbe@gmail.com>
2021-05-20 03:29:21 +0900

    11654: solve in C++11

---
+++ problem/11654/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+char ch;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> ch;
+    cout << static_cast<int>(ch) << "\n";
+    return 0;
+}
