72c0bf0c8d25a6cccf9d4fad91a3d3f07d4058a0
Chayoung You <yousbe@gmail.com>
2021-05-20 03:21:24 +0900

    10998: solve in C++11

---
+++ problem/10998/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int A, B;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> A >> B;
+    cout << A * B << "\n";
+    return 0;
+}
