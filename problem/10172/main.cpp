1086a3968c643743a0f2df9f7d99c747a559bce4
Chayoung You <yousbe@gmail.com>
2021-05-20 02:56:45 +0900

    10172: solve in C++11

---
+++ problem/10172/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const string dog =
+"|\\_/|\n"
+"|q p|   /}\n"
+"( 0 )\"\"\"\\\n"
+"|\"^\"`    |\n"
+"||_/=\\\\__|\n";
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << dog;
+    return 0;
+}
