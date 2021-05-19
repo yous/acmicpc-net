11b2f1ed0cf420b0ab8589205288811af2a5162f
Chayoung You <yousbe@gmail.com>
2021-05-20 03:18:00 +0900

    10951: solve in C++11

---
+++ problem/10951/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int A, B;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while (cin >> A >> B) {
+        cout << A + B << "\n";
+    }
+    return 0;
+}
