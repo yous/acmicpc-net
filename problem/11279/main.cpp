9a789ef87db89db73ff6f568bd70c2b207bd2e9f
Chayoung You <yousbe@gmail.com>
2021-05-22 20:34:45 +0900

    11279: solve in C++11

---
+++ problem/11279/main.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int N;
+priority_queue<int> pq;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N;
+    int num;
+    while (N-- > 0) {
+        cin >> num;
+        if (num == 0) {
+            if (pq.empty()) {
+                cout << "0\n";
+            } else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(num);
+        }
+    }
+    return 0;
+}
