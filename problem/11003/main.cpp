439a1f4d068d58515794ec5a73994a8efc8fee53
Chayoung You <yousbe@gmail.com>
2021-05-22 21:42:33 +0900

    11003: reduce time by checking index of top

--- problem/11003/main.cpp
+++ problem/11003/main.cpp
@@ -5,24 +5,9 @@
 
 using namespace std;
 
-template<typename T, class Container=vector<T>, class Compare=less<typename Container::value_types>>
-class custom_priority_queue : public priority_queue<T, Container, Compare> {
-  public:
-    bool remove(const T& value) {
-        auto it = find(this->c.begin(), this->c.end(), value);
-        if (it != this->c.end()) {
-            this->c.erase(it);
-            make_heap(this->c.begin(), this->c.end(), this->comp);
-            return true;
-        } else {
-            return false;
-        }
-    }
-};
-
 int N, L;
 vector<int> nums;
-custom_priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -33,9 +18,9 @@ int main() {
     for (i = 0; i < N; i++) {
         cin >> num;
         nums.push_back(num);
-        pq.push(make_pair(num, i));
-        if (i >= L) {
-            pq.remove(make_pair(nums[i - L], i - L));
+        pq.push({num, i});
+        while (pq.top().second <= i - L) {
+            pq.pop();
         }
         cout << pq.top().first;
         if (i < N - 1) {
