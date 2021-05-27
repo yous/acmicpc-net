11b0b6f8263ba49bfa022222c3cee944f7d05cea
Chayoung You <yousbe@gmail.com>
2021-05-28 00:03:27 +0900

    2751: reduce space

--- problem/2751/main.cpp
+++ problem/2751/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int N;
-int nums[2000001];
+bool nums[2000001];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -14,11 +14,10 @@ int main() {
     int num;
     while (N-- > 0) {
         cin >> num;
-        nums[num + 1000000]++;
+        nums[num + 1000000] = true;
     }
     for (int i = 0; i <= 2000000; i++) {
-        int count = nums[i];
-        for (int j = 0; j < count; j++) {
+        if (nums[i]) {
             cout << i - 1000000 << "\n";
         }
     }
