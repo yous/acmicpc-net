e59bdeb45c21142531a826c5eb3b7c616dd9e021
Chayoung You <yousbe@gmail.com>
2021-05-31 00:42:49 +0900

    10814: solve in C++11

---
+++ problem/10814/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<tuple<int, int, string>> people;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N;
+    int age;
+    int idx = 0;
+    string name;
+    while (N-- > 0) {
+        cin >> age >> name;
+        people.push_back({age, idx, name});
+        idx++;
+    }
+    sort(people.begin(), people.end());
+    for (auto person : people) {
+        cout << get<0>(person) << " " << get<2>(person) << "\n";
+    }
+    return 0;
+}
