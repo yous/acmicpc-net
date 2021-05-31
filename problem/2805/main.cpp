e293ecfaeaeff77a14785149d03e598f26153a64
Chayoung You <yousbe@gmail.com>
2021-05-31 20:25:05 +0900

    2805: reduce time complexity

--- problem/2805/main.cpp
+++ problem/2805/main.cpp
@@ -8,10 +8,11 @@ int N, M;
 vector<int> trees;
 
 long long length_under(int limit) {
-    auto up_it = upper_bound(trees.begin(), trees.end(), limit);
     long long sum = 0;
-    for (auto it = up_it; it != trees.end(); it++) {
-        sum += *it - limit;
+    for (auto it = trees.begin(); it != trees.end(); it++) {
+        if (*it > limit) {
+            sum += *it - limit;
+        }
     }
     return sum;
 }
@@ -25,9 +26,8 @@ int main() {
         cin >> num;
         trees.push_back(num);
     }
-    sort(trees.begin(), trees.end());
     int lo = 0;
-    int hi = *prev(trees.end());
+    int hi = *max_element(trees.begin(), trees.end());
     while (lo <= hi) {
         int mid = (lo + hi) / 2;
         if (length_under(mid) < M) {
