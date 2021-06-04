1be92ea9b9f884796602c2d10f740b32f6cb73f4
Chayoung You <yousbe@gmail.com>
2021-06-05 05:10:55 +0900

    11727: solve in C++11

---
+++ problem/11727/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MOD = 10007;
+int N;
+int dp[1001];
+
+int solve(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    if (n == 2) {
+        return 3;
+    }
+    int& ans = dp[n];
+    if (ans) {
+        return ans;
+    }
+    ans = (solve(n - 1) + solve(n - 2) * 2 % MOD) % MOD;
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N;
+    cout << solve(N) << "\n";
+    return 0;
+}
