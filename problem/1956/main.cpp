50423c08add3e110226ee5ffa52757f8a2e0a65d
Chayoung You <yousbe@gmail.com>
2021-06-06 22:32:29 +0900

    1956: reduce time by using Floyd-Warshall instead of Dijkstra

--- problem/1956/main.cpp
+++ problem/1956/main.cpp
@@ -1,55 +1,37 @@
 #include <algorithm>
 #include <iostream>
-#include <queue>
-#include <set>
-#include <tuple>
 #include <vector>
 
 using namespace std;
 
-const int INF = 987654321;
+const int INF = 1e9;
 int V, E;
-vector<vector<pair<int, int>>> adj(401);
 int dist[401][401];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> V >> E;
+    for (int i = 0; i < V; i++) {
+        fill(dist[i], dist[i] + V, INF);
+    }
     for (int i = 0; i < E; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        adj[a].push_back({b, c});
+        dist[a - 1][b - 1] = c;
     }
-    for (int i = 1; i <= V; i++) {
-        fill(dist[i] + 1, dist[i] + V + 1, INF);
-        priority_queue<pair<int, int>> pq;
-        for (auto& e : adj[i]) {
-            int v, w;
-            tie(v, w) = e;
-            dist[i][v] = w;
-            pq.push({-w, v});
-        }
-        while (!pq.empty()) {
-            int cost, here;
-            tie(cost, here) = pq.top();
-            cost = -cost;
-            pq.pop();
-            if (dist[i][here] < cost) {
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            if (dist[i][k] == INF) {
                 continue;
             }
-            for (auto& e : adj[here]) {
-                int v, w;
-                tie(v, w) = e;
-                if (dist[i][here] + w < dist[i][v]) {
-                    dist[i][v] = dist[i][here] + w;
-                    pq.push({-dist[i][v], v});
-                }
+            for (int j = 0; j < V; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
     int _min = INF;
-    for (int i = 1; i <= V; i++) {
+    for (int i = 0; i < V; i++) {
         if (dist[i][i] < _min) {
             _min = dist[i][i];
         }
