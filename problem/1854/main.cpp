e78e7f7de2aeef9ed7d67a4ff99105599503e78d
Chayoung You <yousbe@gmail.com>
2021-06-08 17:38:04 +0900

    1854: reduce time, memory using priority_queue

--- problem/1854/main.cpp
+++ problem/1854/main.cpp
@@ -8,8 +8,7 @@ using namespace std;
 
 int N, M, K;
 vector<vector<pair<int, int>>> adj(1001);
-vector<int> dist(1001);
-vector<int> nth(1001);
+vector<priority_queue<int>> dist(1001);
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -22,28 +21,29 @@ int main() {
     }
     priority_queue<pair<int, int>> pq;
     pq.push({0, 1});
+    dist[1].push(0);
     while (!pq.empty()) {
         int cost, here;
         tie(cost, here) = pq.top();
         cost = -cost;
         pq.pop();
-        if (nth[here] < K) {
-            nth[here]++;
-            dist[here] = cost;
-            for (auto& p : adj[here]) {
-                int there, w;
-                tie(there, w) = p;
-                if (nth[there] < K) {
-                    pq.push({-(cost + w), there});
+        for (auto& p : adj[here]) {
+            int there, w;
+            tie(there, w) = p;
+            if (dist[there].size() < K || dist[there].top() > cost + w) {
+                if (dist[there].size() == K) {
+                    dist[there].pop();
                 }
+                dist[there].push(cost + w);
+                pq.push({-(cost + w), there});
             }
         }
     }
     for (int i = 1; i <= N; i++) {
-        if (nth[i] < K) {
+        if (dist[i].size() < K) {
             cout << "-1\n";
         } else {
-            cout << dist[i] << "\n";
+            cout << dist[i].top() << "\n";
         }
     }
     return 0;
