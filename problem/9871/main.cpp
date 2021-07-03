4d85baf15cef4814339b29b5cf8f634223898fca
Chayoung You <yousbe@gmail.com>
2021-07-03 20:54:08 +0900

    9871: reduce time by using sparse table

--- problem/9871/main.cpp
+++ problem/9871/main.cpp
@@ -5,32 +5,50 @@
 using namespace std;
 
 int N, M, Q;
-vector<int> P;
-vector<int> inv_P;
+vector<int> inv_P[32];
 
 int solve(int loc) {
     int start = min(loc, N - M);
-    int ans = loc;
+    int ans = loc - start;
     while (start >= 0) {
-        ans = start + inv_P[ans - start];
-        if (ans >= start + M - 1) {
+        for (int idx = 31; idx >= 0; idx--) {
+            if (start & (1 << idx)) {
+                int n = inv_P[idx][ans];
+                if (n == M) {
+                    continue;
+                }
+                ans = n;
+                start -= (1 << idx);
+            }
+        }
+        ans = inv_P[0][ans];
+        if (ans == M || start == 0) {
             break;
         }
         start--;
     }
-    return ans + 1;
+    return start + ans;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N >> M >> Q;
-    inv_P.resize(M);
+    inv_P[0].resize(M);
     for (int i = 0; i < M; i++) {
         int num;
         cin >> num;
-        P.push_back(num - 1);
-        inv_P[num - 1] = i;
+        inv_P[0][num - 1] = i + 1;
+    }
+    for (int p = 1; p < 32; p++) {
+        for (int i = 0; i < M; i++) {
+            int n = inv_P[p - 1][i];
+            if (n == M) {
+                inv_P[p].push_back(n);
+            } else {
+                inv_P[p].push_back(inv_P[p - 1][n]);
+            }
+        }
     }
     for (int i = 0; i < Q; i++) {
         int num;
