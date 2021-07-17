87986d72f405ce41701679b943fb1f355e148b1e
Chayoung You <yousbe@gmail.com>
2021-07-17 20:44:58 +0900

    5639: improve time, memory by using binary search

--- problem/5639/main.cpp
+++ problem/5639/main.cpp
@@ -1,19 +1,37 @@
 #include <algorithm>
 #include <iostream>
-#include <tuple>
 #include <vector>
 
 using namespace std;
 
 vector<int> preorder;
-vector<tuple<int, int, int>> tree;
+vector<pair<int, int>> tree;
+
+void find_child(int root, int lo, int hi) {
+    if (lo >= hi) {
+        return;
+    }
+    int right_idx = lower_bound(preorder.begin() + lo, preorder.begin() + hi, preorder[root]) - preorder.begin();
+    if (right_idx == lo) {
+        tree[root].second = right_idx;
+        find_child(right_idx, right_idx + 1, hi);
+    } else if (right_idx < hi) {
+        tree[root].first = lo;
+        tree[root].second = right_idx;
+        find_child(lo, lo + 1, right_idx);
+        find_child(right_idx, right_idx + 1, hi);
+    } else {
+        tree[root].first = lo;
+        find_child(lo, lo + 1, hi);
+    }
+}
 
 void postorder(int idx) {
     if (idx == -1) {
         return;
     }
-    postorder(get<1>(tree[idx]));
-    postorder(get<2>(tree[idx]));
+    postorder(tree[idx].first);
+    postorder(tree[idx].second);
     cout << preorder[idx] << "\n";
 }
 
@@ -25,30 +43,8 @@ int main() {
         preorder.push_back(num);
     }
     int sz = preorder.size();
-    tree.resize(sz, {-1, -1, -1});
-    num = preorder[0];
-    for (int i = 1; i < sz; i++) {
-        int next_num = preorder[i];
-        if (next_num < num) {
-            get<1>(tree[i - 1]) = i;
-            get<0>(tree[i]) = i - 1;
-        } else {
-            int idx = i - 1;
-            int parent_idx = get<0>(tree[idx]);
-            while (parent_idx != -1 && preorder[parent_idx] < next_num) {
-                idx = parent_idx;
-                parent_idx = get<0>(tree[idx]);
-            }
-            int right_idx = get<2>(tree[idx]);
-            while (right_idx != -1) {
-                idx = right_idx;
-                right_idx = get<2>(tree[idx]);
-            }
-            get<2>(tree[idx]) = i;
-            get<0>(tree[i]) = idx;
-        }
-        num = next_num;
-    }
+    tree.resize(sz, {-1, -1});
+    find_child(0, 1, sz);
     postorder(0);
     return 0;
 }
