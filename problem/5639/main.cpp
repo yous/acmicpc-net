c73e6374f5bf681528d5fe051f00bd192576ff14
Chayoung You <yousbe@gmail.com>
2021-07-18 17:06:56 +0900

    5639: reduce time, memory by using O(N) algorithm

--- problem/5639/main.cpp
+++ problem/5639/main.cpp
@@ -4,47 +4,24 @@
 
 using namespace std;
 
-vector<int> preorder;
-vector<pair<int, int>> tree;
-
-void find_child(int root, int lo, int hi) {
-    if (lo >= hi) {
-        return;
+void postorder(int root, int limit, int& num) {
+    if (cin >> num) {
+        if (num < root) {
+            postorder(num, root, num);
+        }
+        if (num > root && num < limit) {
+            postorder(num, limit, num);
+        }
     }
-    int right_idx = lower_bound(preorder.begin() + lo, preorder.begin() + hi, preorder[root]) - preorder.begin();
-    if (right_idx == lo) {
-        tree[root].second = right_idx;
-        find_child(right_idx, right_idx + 1, hi);
-    } else if (right_idx < hi) {
-        tree[root].first = lo;
-        tree[root].second = right_idx;
-        find_child(lo, lo + 1, right_idx);
-        find_child(right_idx, right_idx + 1, hi);
-    } else {
-        tree[root].first = lo;
-        find_child(lo, lo + 1, hi);
-    }
-}
-
-void postorder(int idx) {
-    if (idx == -1) {
-        return;
-    }
-    postorder(tree[idx].first);
-    postorder(tree[idx].second);
-    cout << preorder[idx] << "\n";
+    cout << root << "\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    int root;
+    cin >> root;
     int num;
-    while (cin >> num) {
-        preorder.push_back(num);
-    }
-    int sz = preorder.size();
-    tree.resize(sz, {-1, -1});
-    find_child(0, 1, sz);
-    postorder(0);
+    postorder(root, static_cast<int>(1e6), num);
     return 0;
 }
