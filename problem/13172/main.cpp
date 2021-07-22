7f8b636d66e736ebc350260c6985cb3923a69be7
Chayoung You <yousbe@gmail.com>
2021-07-23 03:45:18 +0900

    13172: refactor mod_pow function

--- problem/13172/main.cpp
+++ problem/13172/main.cpp
@@ -7,19 +7,16 @@ using namespace std;
 const int MOD = static_cast<int>(1e9) + 7;
 int M;
 
-int power(int n, int order) {
-    if (order == 0) {
-        return 1;
+int mod_pow(int a, int n) {
+    int res = 1;
+    while (n > 0) {
+        if (n & 1) {
+            res = 1LL * res * a % MOD;
+        }
+        a = 1LL * a * a % MOD;
+        n /= 2;
     }
-    int pow_2 = power(n, order / 2);
-    if (order % 2) {
-        return (1LL * pow_2 * pow_2 % MOD) * n % MOD;
-    }
-    return 1LL * pow_2 * pow_2 % MOD;
-}
-
-int inv(int n) {
-    return power(n, MOD - 2);
+    return res;
 }
 
 int main() {
@@ -30,7 +27,7 @@ int main() {
     for (int i = 0; i < M; i++) {
         int n, s;
         cin >> n >> s;
-        sum += 1LL * s * inv(n) % MOD;
+        sum += 1LL * s * mod_pow(n, MOD - 2) % MOD;
         sum %= MOD;
     }
     cout << sum << "\n";
