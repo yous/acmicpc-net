07c651487d0b7f00284e4b47371ba40f59d6392f
Chayoung You <yousbe@gmail.com>
2021-07-23 02:58:26 +0900

    12865: reduce time, memory by iterative algorithm

--- problem/12865/main.cpp
+++ problem/12865/main.cpp
@@ -5,44 +5,23 @@
 using namespace std;
 
 int N, K;
-vector<int> weights;
-vector<short> values;
-vector<vector<int>> cache;
-
-int solve(int idx, int remain) {
-    if (idx == N || remain == 0) {
-        return 0;
-    }
-    int& ans = cache[idx][remain - 1];
-    if (ans >= 0) {
-        return ans;
-    }
-    ans = solve(idx + 1, remain);
-    int weight = weights[idx];
-    if (remain >= weight) {
-        ans = max(ans, values[idx] + solve(idx + 1, remain - weight));
-    }
-    return ans;
-}
+vector<int> cache;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N >> K;
-    int pass = 0;
+    cache.resize(K + 1);
     for (int i = 0; i < N; i++) {
         int w;
         short v;
         cin >> w >> v;
-        if (v == 0) {
-            pass++;
-            continue;
+        for (int remain = K; remain >= w; remain--) {
+            if (cache[remain - w] + v > cache[remain]) {
+                cache[remain] = cache[remain - w] + v;
+            }
         }
-        weights.push_back(w);
-        values.push_back(v);
     }
-    N -= pass;
-    cache.resize(N, vector<int>(K, -1));
-    cout << solve(0, K) << "\n";
+    cout << cache[K] << "\n";
     return 0;
 }
