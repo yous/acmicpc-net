9999ebeac73feb47a6c6239e1e236479330d5384
Chayoung You <yousbe@gmail.com>
2021-07-23 02:35:45 +0900

    12865: solve in C++11

---
+++ problem/12865/main.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, K;
+vector<int> weights;
+vector<short> values;
+vector<vector<int>> cache;
+
+int solve(int idx, int remain) {
+    if (idx == N || remain == 0) {
+        return 0;
+    }
+    int& ans = cache[idx][remain - 1];
+    if (ans >= 0) {
+        return ans;
+    }
+    ans = solve(idx + 1, remain);
+    int weight = weights[idx];
+    if (remain >= weight) {
+        ans = max(ans, values[idx] + solve(idx + 1, remain - weight));
+    }
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N >> K;
+    cache.resize(N, vector<int>(K, -1));
+    for (int i = 0; i < N; i++) {
+        int w;
+        short v;
+        cin >> w >> v;
+        weights.push_back(w);
+        values.push_back(v);
+    }
+    cout << solve(0, K) << "\n";
+    return 0;
+}
