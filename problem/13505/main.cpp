9d7ef1fd901e5c87795c35eb8fa8cb0bbb0d3e2b
Chayoung You <yousbe@gmail.com>
2021-07-31 17:23:06 +0900

    13505: reduce time, memory by changing vector layout

--- problem/13505/main.cpp
+++ problem/13505/main.cpp
@@ -11,16 +11,16 @@ struct Trie {
     vector<vector<int>> t;
     int current_idx = 1;
 
-    Trie(int N) : t(N * 30, vector<int>(2)) {}
+    Trie(int N) : t(2, vector<int>(N * 30)) {}
 
     void insert(int n) {
         int idx = 0;
         for (int p = 29; p >= 0; p--) {
             int pos = (n & (1 << p)) >> p;
-            int new_idx = t[idx][pos];
+            int new_idx = t[pos][idx];
             if (new_idx == 0) {
-                t[idx][pos] = current_idx++;
-                new_idx = t[idx][pos];
+                t[pos][idx] = current_idx++;
+                new_idx = t[pos][idx];
             }
             idx = new_idx;
         }
@@ -31,10 +31,10 @@ struct Trie {
         int idx = 0;
         for (int p = 29; p >= 0; p--) {
             int pos = (n & (1 << p)) >> p;
-            int new_idx = t[idx][pos];
+            int new_idx = t[pos][idx];
             if (new_idx == 0) {
                 pos ^= 1;
-                new_idx = t[idx][pos];
+                new_idx = t[pos][idx];
             }
             ans += (pos << p);
             idx = new_idx;
