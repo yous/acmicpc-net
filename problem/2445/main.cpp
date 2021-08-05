090ebf6ad7ec275b6c26ff4d100e2f08b911e0e0
Chayoung You <yousbe@gmail.com>
2021-08-06 02:46:43 +0900

    2445: solve in C++17

---
+++ problem/2445/main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cout << string(i, '*') << string((N - i) * 2, ' ') << string(i, '*') << "\n";
+    }
+    for (int i = N - 1; i >= 1; i--) {
+        cout << string(i, '*') << string((N - i) * 2, ' ') << string(i, '*') << "\n";
+    }
+    return 0;
+}
