48eb15e893dd23d5b59300fde2bf1690df5e4459
Chayoung You <yousbe@gmail.com>
2021-08-09 16:23:39 +0900

    1929: reduce time, memory by generating sieve

--- problem/1929/main.cpp
+++ problem/1929/main.cpp
@@ -5,35 +5,28 @@
 using namespace std;
 
 int M, N;
-vector<int> primes;
+vector<bool> sieve;
+
+void gen_sieve(int limit) {
+    sieve[1] = false;
+    for (int i = 2; i * i <= limit; i++) {
+        if (sieve[i]) {
+            for (int j = i * i; j <= limit; j += i) {
+                sieve[j] = false;
+            }
+        }
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> M >> N;
-    primes.push_back(2);
-    if (M <= 2 && N >= 2) {
-        cout << "2\n";
-    }
-    if (M % 2 == 0) {
-        M++;
-    }
-    for (int i = 3; i <= N; i += 2) {
-        bool is_prime = true;
-        for (int prime : primes) {
-            if (prime * prime > i) {
-                break;
-            }
-            if (i % prime == 0) {
-                is_prime = false;
-                break;
-            }
-        }
-        if (is_prime) {
-            primes.push_back(i);
-            if (i >= M) {
-                cout << i << "\n";
-            }
+    sieve.resize(N + 1, true);
+    gen_sieve(N);
+    for (int i = M; i <= N; i++) {
+        if (sieve[i]) {
+            cout << i << "\n";
         }
     }
     return 0;
