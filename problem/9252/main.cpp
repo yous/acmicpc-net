c7cf360ff7def4a917dbe7d2a936d69ac1f4f895
Chayoung You <yousbe@gmail.com>
2021-08-10 02:30:08 +0900

    9252: reduce time using iterative algorithm

--- problem/9252/main.cpp
+++ problem/9252/main.cpp
@@ -9,41 +9,43 @@ int sz1, sz2;
 vector<vector<short>> cache;
 vector<vector<char>> next_idx;
 
-short solve(int idx1, int idx2) {
-    if (idx1 == sz1 || idx2 == sz2) {
-        return 0;
-    }
-    short& ans = cache[idx1][idx2];
-    char& nxt = next_idx[idx1][idx2];
-    if (ans >= 0) {
-        return ans;
-    }
-    if (S1[idx1] == S2[idx2]) {
-        ans = 1 + solve(idx1 + 1, idx2 + 1);
-        nxt = 3;
-    } else {
-        int nxt1 = solve(idx1 + 1, idx2);
-        int nxt2 = solve(idx1, idx2 + 1);
-        if (nxt1 >= nxt2) {
-            ans = nxt1;
-            nxt = 1;
-        } else {
-            ans = nxt2;
-            nxt = 2;
-        }
-    }
-    return ans;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> S1 >> S2;
     sz1 = S1.size();
     sz2 = S2.size();
-    cache.resize(sz1, vector<short>(sz2, -1));
+    cache.resize(sz1, vector<short>(sz2));
     next_idx.resize(sz1, vector<char>(sz2));
-    int len = solve(0, 0);
+    for (int i = sz1 + sz2 - 2; i >= 0; i--) {
+        for (int idx1 = min(sz1 - 1, i); idx1 >= max(0, i - sz2 + 1); idx1--) {
+            int idx2 = i - idx1;
+            if (S1[idx1] == S2[idx2]) {
+                cache[idx1][idx2] = 1;
+                if (idx1 < sz1 - 1 && idx2 < sz2 - 1) {
+                    cache[idx1][idx2] += cache[idx1 + 1][idx2 + 1];
+                }
+                next_idx[idx1][idx2] = 3;
+            } else {
+                short nxt1 = 0;
+                if (idx1 < sz1 - 1) {
+                    nxt1 = cache[idx1 + 1][idx2];
+                }
+                short nxt2 = 0;
+                if (idx2 < sz2 - 1) {
+                    nxt2 = cache[idx1][idx2 + 1];
+                }
+                if (nxt1 >= nxt2) {
+                    cache[idx1][idx2] = nxt1;
+                    next_idx[idx1][idx2] = 1;
+                } else {
+                    cache[idx1][idx2] = nxt2;
+                    next_idx[idx1][idx2] = 2;
+                }
+            }
+        }
+    }
+    short len = cache[0][0];
     cout << len << "\n";
     if (len > 0) {
         int idx1 = 0;
