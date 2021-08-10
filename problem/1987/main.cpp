dd0eeab1747260f2d3fc05157c3ff12090a32408
Chayoung You <yousbe@gmail.com>
2021-08-11 02:51:45 +0900

    1987: reduce time by removing visited

--- problem/1987/main.cpp
+++ problem/1987/main.cpp
@@ -9,21 +9,18 @@ const int dy[] = {-1, 1, 0, 0};
 const int dx[] = {0, 0, -1, 1};
 int R, C;
 vector<string> board;
-vector<vector<bool>> visited;
 
 int solve(int y, int x, bitset<26>& chars) {
     int ans = 1;
     for (int i = 0; i < 4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if (ny < 0 || ny >= R || nx < 0 || nx >= C || visited[ny][nx] || chars[board[ny][nx] - 'a']) {
+        if (ny < 0 || ny >= R || nx < 0 || nx >= C || chars[board[ny][nx] - 'A']) {
             continue;
         }
-        visited[ny][nx] = true;
-        chars[board[ny][nx] - 'a'] = true;
+        chars[board[ny][nx] - 'A'] = true;
         ans = max(ans, 1 + solve(ny, nx, chars));
-        visited[ny][nx] = false;
-        chars[board[ny][nx] - 'a'] = false;
+        chars[board[ny][nx] - 'A'] = false;
     }
     return ans;
 }
@@ -33,13 +30,11 @@ int main() {
     cin.tie(nullptr);
     cin >> R >> C;
     board.resize(R);
-    visited.resize(R, vector<bool>(C));
     for (int i = 0; i < R; i++) {
         cin >> board[i];
     }
     bitset<26> chars;
-    visited[0][0] = true;
-    chars[board[0][0] - 'a'] = true;
+    chars[board[0][0] - 'A'] = true;
     cout << solve(0, 0, chars) << "\n";
     return 0;
 }
