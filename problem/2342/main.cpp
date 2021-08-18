9c865dd30837eaf2b72e424b47884426b2b18aca
Chayoung You <yousbe@gmail.com>
2021-08-19 02:59:13 +0900

    2342: reduce time, memory by toggling

--- problem/2342/main.cpp
+++ problem/2342/main.cpp
@@ -5,9 +5,6 @@
 using namespace std;
 
 const int INF = 1e9;
-vector<char> STEPS;
-int SZ;
-vector<int> cache[5][5];
 const int dist[5][4] = {
     {2, 2, 2, 2},
     {1, 3, 4, 3},
@@ -15,43 +12,46 @@ const int dist[5][4] = {
     {4, 3, 1, 3},
     {3, 4, 3, 1}
 };
-
-int solve(int idx, int l, int r) {
-    if (idx >= SZ) {
-        return 0;
-    }
-    int& ans = cache[l][r][idx];
-    if (ans < INF) {
-        return ans;
-    }
-    int step = STEPS[idx];
-    if (l == step) {
-        ans = 1 + solve(idx + 1, l, r);
-        return ans;
-    } else if (r == step) {
-        ans = 1 + solve(idx + 1, l, r);
-        return ans;
-    }
-    ans = min({ans, dist[l][step - 1] + solve(idx + 1, step, r), dist[r][step - 1] + solve(idx + 1, l, step)});
-    return ans;
-}
+int dp[2][5][5];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    for (int i = 0; i < 5; i++) {
+        fill(dp[0][i], dp[0][i] + 5, INF);
+        fill(dp[1][i], dp[1][i] + 5, INF);
+    }
+    int idx = 0;
+    dp[idx][0][0] = 0;
     char ch;
     while (cin >> ch) {
         if (ch == '0') {
             break;
         }
-        STEPS.push_back(ch - '0');
+        int step = ch - '0';
+        for (int i = 0; i < 5; i++) {
+            fill(dp[idx ^ 1][i], dp[idx ^ 1][i] + 5, INF);
+        }
+        for (int l = 0; l < 5; l++) {
+            for (int r = 0; r < 5; r++) {
+                if (dp[idx][l][r] < INF) {
+                    if (r != step) {
+                        dp[idx ^ 1][step][r] = min(dp[idx ^ 1][step][r], dist[l][step - 1] + dp[idx][l][r]);
+                    }
+                    if (l != step) {
+                        dp[idx ^ 1][l][step] = min(dp[idx ^ 1][l][step], dist[r][step - 1] + dp[idx][l][r]);
+                    }
+                }
+            }
+        }
+        idx ^= 1;
     }
-    SZ = STEPS.size();
+    int ans = INF;
     for (int l = 0; l < 5; l++) {
         for (int r = 0; r < 5; r++) {
-            cache[l][r].resize(SZ, INF);
+            ans = min(ans, dp[idx][l][r]);
         }
     }
-    cout << solve(0, 0, 0) << "\n";
+    cout << ans << "\n";
     return 0;
 }
