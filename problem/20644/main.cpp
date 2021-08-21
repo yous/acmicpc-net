19a2617a92a51fa9932092d17ae9a713b709ab64
Chayoung You <yousbe@gmail.com>
2021-08-22 04:38:45 +0900

    20644: reduce time, memory by removing variables

--- problem/20644/main.cpp
+++ problem/20644/main.cpp
@@ -10,17 +10,12 @@ const int dx[] = {0, 0, -1, 1};
 int N, D;
 vector<string> farm;
 vector<pair<int, int>> starts;
-vector<vector<bool>> visited[500];
-vector<vector<bool>> failed[500];
 vector<vector<int>> max_sizes;
 vector<vector<int>> visited_diff;
 vector<vector<int>> y_plus_x;
 vector<vector<int>> y_minus_x;
 
 bool check(int y, int x, int sz, int dir) {
-    if (failed[sz][y][x]) {
-        return false;
-    }
     switch (dir) {
         case 0:
             return y_plus_x[y - sz + x][y - sz] >= sz + 1 &&
@@ -42,7 +37,6 @@ void bfs() {
     queue<tuple<int, int, int>> qu;
     int time = 1;
     for (auto [sy, sx] : starts) {
-        visited[0][sy][sx] = true;
         max_sizes[sy][sx] = 0;
         qu.emplace(sy, sx, 0);
     }
@@ -57,22 +51,17 @@ void bfs() {
                 if (farm[ny][nx] == '#') {
                     continue;
                 }
-                if (visited[size][ny][nx]) {
+                if (max_sizes[ny][nx] >= size) {
                     continue;
                 }
                 if (!check(ny, nx, size, i)) {
-                    failed[size][ny][nx] = true;
                     continue;
                 }
-                visited[size][ny][nx] = true;
                 max_sizes[ny][nx] = size;
                 if (time % D == 0) {
                     if (check(ny, nx, size + 1, i) && check(ny, nx, size + 1, i ^ 1)) {
-                        visited[size + 1][ny][nx] = true;
                         max_sizes[ny][nx] = size + 1;
                         qu.emplace(ny, nx, size + 1);
-                    } else {
-                        failed[size + 1][ny][nx] = true;
                     }
                 } else {
                     qu.emplace(ny, nx, size);
@@ -87,10 +76,6 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N >> D;
-    for (int i = 0; i < 500; i++) {
-        visited[i].resize(N, vector<bool>(N));
-        failed[i].resize(N, vector<bool>(N));
-    }
     max_sizes.resize(N, vector<int>(N, -1));
     visited_diff.resize(N, vector<int>(N));
     y_plus_x.resize(N * 2 - 1);
