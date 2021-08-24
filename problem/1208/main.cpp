caed33b663086c0a5bc351ea5d8bcd6199d01a48
Chayoung You <yousbe@gmail.com>
2021-08-25 04:08:59 +0900

    1208: reduce time by using brute force

--- problem/1208/main.cpp
+++ problem/1208/main.cpp
@@ -10,34 +10,15 @@ vector<int> right_nums;
 vector<int> left_counts;
 vector<int> right_counts;
 
-int solve(vector<int>& nums, vector<int>& counts, int min_val, int max_val) {
-    int sz = nums.size();
-    int offset = min_val;
-    min_val = 0;
-    max_val = 0;
-    for (int i = 0; i < sz; i++) {
-        int num = nums[i];
-        if (num >= 0) {
-            for (int n = max_val - offset; n >= min_val - offset; n--) {
-                counts[n + num] += counts[n];
-            }
-        } else {
-            for (int n = min_val - offset; n <= max_val - offset; n++) {
-                counts[n + num] += counts[n];
-            }
+void solve(int idx, int cnt, int sz, vector<int>& nums, vector<int>& counts, int sum) {
+    if (idx == sz) {
+        if (cnt > 0) {
+            counts[sum]++;
         }
-        counts[num - offset]++;
-        if (num >= 0) {
-            max_val += num;
-        } else {
-            min_val += num;
-        }
+        return;
     }
-    if (S < min_val || S > max_val) {
-        return 0;
-    } else {
-        return counts[S - offset];
-    }
+    solve(idx + 1, cnt, sz, nums, counts, sum);
+    solve(idx + 1, cnt + 1, sz, nums, counts, sum + nums[idx]);
 }
 
 int main() {
@@ -68,6 +49,7 @@ int main() {
     }
     int left_sz = left_max_val - left_min_val + 1;
     left_counts.resize(left_sz);
+    solve(0, 0, N / 2, left_nums, left_counts, -left_min_val);
     int right_min_val = 0;
     int right_max_val = 0;
     for (int i = N / 2; i < N; i++) {
@@ -82,9 +64,14 @@ int main() {
     }
     int right_sz = right_max_val - right_min_val + 1;
     right_counts.resize(right_sz);
+    solve(0, 0, N - N / 2, right_nums, right_counts, -right_min_val);
     long long ans = 0;
-    ans += solve(left_nums, left_counts, left_min_val, left_max_val);
-    ans += solve(right_nums, right_counts, right_min_val, right_max_val);
+    if (S >= left_min_val && S <= left_max_val) {
+        ans += left_counts[S - left_min_val];
+    }
+    if (S >= right_min_val && S <= right_max_val) {
+        ans += right_counts[S - right_min_val];
+    }
     int left_idx = 0;
     while (left_idx < left_sz && left_counts[left_idx] == 0) {
         left_idx++;
@@ -97,20 +84,17 @@ int main() {
         int sum = left_idx + left_min_val + right_idx + right_min_val;
         if (sum < S) {
             left_idx++;
-            while (left_idx < left_sz && left_counts[left_idx] == 0) {
-                left_idx++;
-            }
         } else if (sum > S) {
             right_idx--;
-            while (right_idx >= 0 && right_counts[right_idx] == 0) {
-                right_idx--;
-            }
         } else {
             ans += 1LL * left_counts[left_idx] * right_counts[right_idx];
             left_idx++;
-            while (left_idx < left_sz && left_counts[left_idx] == 0) {
-                left_idx++;
-            }
+        }
+        while (left_idx < left_sz && left_counts[left_idx] == 0) {
+            left_idx++;
+        }
+        while (right_idx >= 0 && right_counts[right_idx] == 0) {
+            right_idx--;
         }
     }
     cout << ans << "\n";
