b764b91fc7999cbc31d9d8e8160d156aa793cd94
Chayoung You <yousbe@gmail.com>
2021-09-07 19:45:00 +0900

    16946: reduce time, memory by removing variables

--- problem/16946/main.cpp
+++ problem/16946/main.cpp
@@ -9,7 +9,7 @@ const int dx[] = {0, 0, -1, 1};
 int N, M;
 vector<string> field;
 vector<vector<bool>> visited;
-vector<string> ans;
+vector<vector<bool>> is_wall;
 vector<vector<int>> walls;
 vector<int> labels;
 
@@ -21,9 +21,9 @@ int dfs(int y, int x, int label) {
         if (ny < 0 || ny >= N || nx < 0 || nx >= M || visited[ny][nx]) {
             continue;
         }
-        if (field[ny][nx] != '0') {
+        if (is_wall[ny][nx]) {
             if (walls[ny][nx] != label) {
-                ans[ny][nx] = (ans[ny][nx] - '0' + labels[walls[ny][nx]]) % 10 + '0';
+                field[ny][nx] = (field[ny][nx] - '0' + labels[walls[ny][nx]]) % 10 + '0';
                 walls[ny][nx] = label;
             }
             continue;
@@ -40,17 +40,23 @@ int main() {
     cin >> N >> M;
     field.resize(N);
     visited.resize(N, vector<bool>(M));
-    ans.resize(N);
+    is_wall.resize(N, vector<bool>(M));
     walls.resize(N, vector<int>(M));
     for (int i = 0; i < N; i++) {
         cin >> field[i];
-        ans[i] = field[i];
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (field[i][j] == '1') {
+                is_wall[i][j] = true;
+            }
+        }
     }
     labels.push_back(0);
     int label = 1;
     for (int y = 0; y < N; y++) {
         for (int x = 0; x < M; x++) {
-            if (field[y][x] == '0' && !visited[y][x]) {
+            if (!is_wall[y][x] && !visited[y][x]) {
                 visited[y][x] = true;
                 int cnt = dfs(y, x, label);
                 labels.push_back(cnt);
@@ -60,13 +66,11 @@ int main() {
     }
     for (int y = 0; y < N; y++) {
         for (int x = 0; x < M; x++) {
-            if (field[y][x] == '1') {
-                cout << (ans[y][x] - '0' + labels[walls[y][x]]) % 10;
-            } else {
-                cout << 0;
+            if (is_wall[y][x]) {
+                field[y][x] = (field[y][x] - '0' + labels[walls[y][x]]) % 10 + '0';
             }
         }
-        cout << "\n";
+        cout << field[y] << "\n";
     }
     return 0;
 }
