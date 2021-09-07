28e699f3050afbe1565e2f9423073ff7ef85542e
Chayoung You <yousbe@gmail.com>
2021-09-07 19:37:01 +0900

    reduce time, memory by labeling

--- problem/16946/main.cpp
+++ problem/16946/main.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <set>
 #include <vector>
 
 using namespace std;
@@ -10,46 +9,29 @@ const int dx[] = {0, 0, -1, 1};
 int N, M;
 vector<string> field;
 vector<vector<bool>> visited;
+vector<string> ans;
+vector<vector<int>> walls;
+vector<int> labels;
 
-struct DisjointSet {
-    vector<int> group;
-    vector<int> counts;
-
-    DisjointSet(int n) : group(n), counts(n) {
-        for (int i = 0; i < n; i++) {
-            group[i] = i;
-            counts[i] = 1;
-        }
-    }
-
-    int find(int n) {
-        if (n == group[n]) {
-            return n;
-        }
-        return group[n] = find(group[n]);
-    }
-
-    void merge(int p, int q) {
-        int gp = find(p);
-        int gq = find(q);
-        if (gp != gq) {
-            group[gp] = gq;
-            counts[gq] += counts[gp];
-        }
-    }
-};
-
-void dfs(int y, int x, DisjointSet& ds) {
+int dfs(int y, int x, int label) {
+    int cnt = 1;
     for (int i = 0; i < 4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if (ny < 0 || ny >= N || nx < 0 || nx >= M || field[ny][nx] != '0' || visited[ny][nx]) {
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M || visited[ny][nx]) {
+            continue;
+        }
+        if (field[ny][nx] != '0') {
+            if (walls[ny][nx] != label) {
+                ans[ny][nx] = (ans[ny][nx] - '0' + labels[walls[ny][nx]]) % 10 + '0';
+                walls[ny][nx] = label;
+            }
             continue;
         }
         visited[ny][nx] = true;
-        ds.merge(y * M + x, ny * M + nx);
-        dfs(ny, nx, ds);
+        cnt += dfs(ny, nx, label);
     }
+    return cnt;
 }
 
 int main() {
@@ -58,36 +40,28 @@ int main() {
     cin >> N >> M;
     field.resize(N);
     visited.resize(N, vector<bool>(M));
-    DisjointSet ds(N * M);
+    ans.resize(N);
+    walls.resize(N, vector<int>(M));
     for (int i = 0; i < N; i++) {
         cin >> field[i];
+        ans[i] = field[i];
     }
+    labels.push_back(0);
+    int label = 1;
     for (int y = 0; y < N; y++) {
         for (int x = 0; x < M; x++) {
             if (field[y][x] == '0' && !visited[y][x]) {
                 visited[y][x] = true;
-                dfs(y, x, ds);
+                int cnt = dfs(y, x, label);
+                labels.push_back(cnt);
+                label++;
             }
         }
     }
     for (int y = 0; y < N; y++) {
         for (int x = 0; x < M; x++) {
             if (field[y][x] == '1') {
-                int cnt = 1;
-                set<int> groups;
-                for (int i = 0; i < 4; i++) {
-                    int ny = y + dy[i];
-                    int nx = x + dx[i];
-                    if (ny < 0 || ny >= N || nx < 0 || nx >= M || field[ny][nx] != '0') {
-                        continue;
-                    }
-                    int group = ds.find(ny * M + nx);
-                    if (groups.find(group) == groups.end()) {
-                        cnt += ds.counts[group];
-                        groups.insert(group);
-                    }
-                }
-                cout << cnt % 10;
+                cout << (ans[y][x] - '0' + labels[walls[y][x]]) % 10;
             } else {
                 cout << 0;
             }
