c97f4d0ba187e09b67c504ff96bbc779c805933c
Chayoung You <yousbe@gmail.com>
2021-09-09 02:06:19 +0900

    1509: reduce time by computing palindromes at first

--- problem/1509/main.cpp
+++ problem/1509/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 string S;
 int sz;
+vector<int> palin_odd;
+vector<int> palin_even;
 vector<int> cache;
 
 int solve(int idx) {
@@ -17,20 +19,14 @@ int solve(int idx) {
         return ans;
     }
     ans = 1 + solve(idx + 1);
-    for (int i = idx + 1; i < sz; i++) {
-        int lo = idx;
-        int hi = i;
-        bool found = true;
-        while (lo < hi) {
-            if (S[lo] != S[hi]) {
-                found = false;
-                break;
-            }
-            lo++;
-            hi--;
+    for (int i = 2; i < sz - idx; i += 2) {
+        if (palin_odd[idx + i / 2] >= 1 + i / 2) {
+            ans = min(ans, 1 + solve(idx + i + 1));
         }
-        if (found) {
-            ans = min(ans, 1 + solve(i + 1));
+    }
+    for (int i = 1; i < sz - idx; i += 2) {
+        if (palin_even[idx + i / 2] >= 1 + i / 2) {
+            ans = min(ans, 1 + solve(idx + i + 1));
         }
     }
     return ans;
@@ -41,6 +37,28 @@ int main() {
     cin.tie(nullptr);
     cin >> S;
     sz = S.size();
+    for (int i = 0; i < sz; i++) {
+        int lo = i;
+        int hi = i;
+        int cnt = 0;
+        while (lo >= 0 && hi < sz && S[lo] == S[hi]) {
+            cnt++;
+            lo--;
+            hi++;
+        }
+        palin_odd.push_back(cnt);
+    }
+    for (int i = 0; i < sz - 1; i++) {
+        int lo = i;
+        int hi = i + 1;
+        int cnt = 0;
+        while (lo >= 0 && hi < sz && S[lo] == S[hi]) {
+            cnt++;
+            lo--;
+            hi++;
+        }
+        palin_even.push_back(cnt);
+    }
     cache.resize(sz);
     cout << solve(0) << "\n";
     return 0;
