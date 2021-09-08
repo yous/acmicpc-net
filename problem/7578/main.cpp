19f76997f511c3a05ba10e66cc7e5eb7f420411c
Chayoung You <yousbe@gmail.com>
2021-09-09 04:26:13 +0900

    7578: reduce time, memory by using Fenwick tree

--- problem/7578/main.cpp
+++ problem/7578/main.cpp
@@ -7,30 +7,26 @@ using namespace std;
 int N;
 int machines[1000001];
 
-struct SegTree {
+struct FenwickTree {
     int N;
     vector<int> t;
 
-    SegTree(int n) : N(n), t(N * 2) {}
+    FenwickTree(int n) : N(n), t(n + 1) {}
 
-    void update(int pos, int val) {
-        pos += N;
-        for (t[pos] = val; pos > 1; pos /= 2) {
-            t[pos / 2] = t[pos] + t[pos ^ 1];
+    int sum(int pos) {
+        int ans = 0;
+        while (pos >= 1) {
+            ans += t[pos];
+            pos -= (pos & -pos);
         }
+        return ans;
     }
 
-    int query(int l, int r) {
-        int total = 0;
-        for (l += N, r += N; l < r; l /= 2, r /= 2) {
-            if (l & 1) {
-                total += t[l++];
-            }
-            if (r & 1) {
-                total += t[--r];
-            }
+    void update(int pos, int val) {
+        while (pos <= N) {
+            t[pos] += val;
+            pos += (pos & -pos);
         }
-        return total;
     }
 };
 
@@ -38,7 +34,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N;
-    SegTree st(N);
+    FenwickTree ft(N);
     for (int i = 0; i < N; i++) {
         int num;
         cin >> num;
@@ -49,8 +45,8 @@ int main() {
         int num;
         cin >> num;
         int idx = machines[num];
-        ans += st.query(idx + 1, N);
-        st.update(idx, 1);
+        ans += ft.sum(N - (idx + 1));
+        ft.update(N - idx, 1);
     }
     cout << ans << "\n";
     return 0;
