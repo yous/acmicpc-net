538753b95c3f0f40e7168bd9b4dad19f9ae52996
Chayoung You <yousbe@gmail.com>
2021-09-11 02:36:10 +0900

    1562: reduce time, memory by reducing mask

--- problem/1562/main.cpp
+++ problem/1562/main.cpp
@@ -6,22 +6,24 @@ using namespace std;
 
 const int MOD = 1e9;
 short N;
-int cache[99][10][1 << 10];
+int cache[99][10][100];
 
 int solve(int idx, int num, int mask) {
     if (idx == N - 1) {
-        return (mask == (1 << 10) - 1);
+        return mask == 9;
     }
     int& ans = cache[idx][num][mask];
     if (ans > 0) {
         return ans - 1;
     }
+    int lo = mask / 10;
+    int hi = mask % 10;
     if (num > 0) {
-        ans += solve(idx + 1, num - 1, mask | (1 << (num - 1)));
+        ans += solve(idx + 1, num - 1, min(lo, num - 1) * 10 + hi);
         ans %= MOD;
     }
     if (num < 9) {
-        ans += solve(idx + 1, num + 1, mask | (1 << (num + 1)));
+        ans += solve(idx + 1, num + 1, lo * 10 + max(hi, num + 1));
         ans %= MOD;
     }
     ans++;
@@ -38,7 +40,7 @@ int main() {
     }
     int ans = 0;
     for (int i = 1; i <= 9; i++) {
-        ans += solve(0, i, (1 << i));
+        ans += solve(0, i, i * 11);
         ans %= MOD;
     }
     cout << ans << "\n";
