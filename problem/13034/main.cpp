c4ea0410e10b54c896d7cd9bbd3b476fde62397b
Chayoung You <yousbe@gmail.com>
2021-09-11 18:06:12 +0900

    13034: reduce time by moving variables

--- problem/13034/main.cpp
+++ problem/13034/main.cpp
@@ -5,13 +5,12 @@
 using namespace std;
 
 int N;
-vector<int> grundy;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N;
-    grundy.resize(N + 1);
+    vector<int> grundy(N + 1);
     for (int n = 2; n <= N; n++) {
         vector<bool> actions(n + 1);
         for (int i = 0; i <= (n - 2) / 2; i++) {
