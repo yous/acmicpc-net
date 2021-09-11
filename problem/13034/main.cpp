f3eaa8b50a5bc622e4ee090dc878981c328ad8f0
Chayoung You <yousbe@gmail.com>
2021-09-11 18:03:30 +0900

    13034: solve in C++17

---
+++ problem/13034/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> grundy;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N;
+    grundy.resize(N + 1);
+    for (int n = 2; n <= N; n++) {
+        vector<bool> actions(n + 1);
+        for (int i = 0; i <= (n - 2) / 2; i++) {
+            actions[grundy[i] ^ grundy[n - 2 - i]] = true;
+        }
+        grundy[n] = distance(actions.begin(), min_element(actions.begin(), actions.end()));
+    }
+    cout << (grundy[N] == 0 ? 2 : 1) << "\n";
+    return 0;
+}
