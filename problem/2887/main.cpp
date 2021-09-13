5cc305eb30ba6fb20c05966a569963dd92531200
Chayoung You <yousbe@gmail.com>
2021-09-14 05:19:27 +0900

    2887: reduce time, memory by using resize

--- problem/2887/main.cpp
+++ problem/2887/main.cpp
@@ -7,10 +7,12 @@ using namespace std;
 
 struct DisjointSet {
     vector<int> group;
+    vector<int> cnt;
 
-    DisjointSet(int n) : group(n) {
+    DisjointSet(int n) : group(n), cnt(n) {
         for (int i = 0; i < n; i++) {
             group[i] = i;
+            cnt[i] = 1;
         }
     }
 
@@ -25,7 +27,13 @@ struct DisjointSet {
         int gp = find(p);
         int gq = find(q);
         if (gp != gq) {
-            group[gp] = gq;
+            if (cnt[gp] < cnt[gq]) {
+                group[gp] = gq;
+                cnt[gq] += cnt[gp];
+            } else {
+                group[gq] = gp;
+                cnt[gp] += cnt[gq];
+            }
             return true;
         }
         return false;
@@ -38,48 +46,57 @@ int main() {
     int N;
     cin >> N;
     vector<pair<int, int>> xyz[3];
+    for (int i = 0; i < 3; i++) {
+        xyz[i].resize(N);
+    }
     DisjointSet ds(N);
     for (int i = 0; i < N; i++) {
         int x, y, z;
         cin >> x >> y >> z;
-        xyz[0].emplace_back(x, i);
-        xyz[1].emplace_back(y, i);
-        xyz[2].emplace_back(z, i);
+        xyz[0][i] = {x, i};
+        xyz[1][i] = {y, i};
+        xyz[2][i] = {z, i};
     }
     for (int i = 0; i < 3; i++) {
         sort(xyz[i].begin(), xyz[i].end());
     }
     vector<tuple<int, int, int>> xyz_diff[3];
     for (int i = 0; i < 3; i++) {
+        xyz_diff[i].resize(N - 1);
         for (int j = 0; j < N - 1; j++) {
             auto [x1, i1] = xyz[i][j];
             auto [x2, i2] = xyz[i][j + 1];
-            xyz_diff[i].emplace_back(abs(x2 - x1), i1, i2);
+            xyz_diff[i][j] = {x2 - x1, i1, i2};
         }
-    }
-    for (int i = 0; i < 3; i++) {
         sort(xyz_diff[i].begin(), xyz_diff[i].end());
     }
     int idx[3] = {0, 0, 0};
     int diffs[3] = {get<0>(xyz_diff[0][0]), get<0>(xyz_diff[1][0]), get<0>(xyz_diff[2][0])};
     long long ans = 0;
+    int cnt = 0;
     while (true) {
         int i;
-        if (idx[0] < N && diffs[0] <= diffs[1] && diffs[0] <= diffs[2]) {
+        if (diffs[0] <= diffs[1] && diffs[0] <= diffs[2]) {
             i = 0;
-        } else if (idx[1] < N && diffs[1] <= diffs[0] && diffs[1] <= diffs[2]) {
+        } else if (diffs[1] <= diffs[0] && diffs[1] <= diffs[2]) {
             i = 1;
-        } else if (idx[2] < N) {
-            i = 2;
         } else {
-            break;
+            i = 2;
         }
         auto [d, i1, i2] = xyz_diff[i][idx[i]];
         if (ds.merge(i1, i2)) {
             ans += d;
+            cnt++;
+            if (cnt == N - 1) {
+                break;
+            }
         }
         idx[i]++;
-        diffs[i] = get<0>(xyz_diff[i][idx[i]]);
+        if (idx[i] < N) {
+            diffs[i] = get<0>(xyz_diff[i][idx[i]]);
+        } else {
+            diffs[i] = static_cast<int>(2e9) + 1;
+        }
     }
     cout << ans << "\n";
     return 0;
