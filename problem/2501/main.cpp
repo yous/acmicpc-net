f1479d805a748138cc085b7ed3aa8601e560ab86
Chayoung You <yousbe@gmail.com>
2021-10-02 16:12:19 +0900

    2501: improve complexity by reducing loop

--- problem/2501/main.cpp
+++ problem/2501/main.cpp
@@ -5,23 +5,31 @@
 using namespace std;
 
 int N, K;
+vector<int> dividers;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N >> K;
     int cnt = 0;
-    for (int i = 1; i <= N; i++) {
+    for (int i = 1; i * i <= N; i++) {
         if (N % i == 0) {
             cnt++;
-        }
-        if (cnt == K) {
-            cout << i << "\n";
-            break;
+            if (cnt == K) {
+                cout << i << "\n";
+                break;
+            }
+            if (i * i != N) {
+                dividers.emplace_back(N / i);
+            }
         }
     }
     if (cnt < K) {
-        cout << "0\n";
+        if (cnt + dividers.size() < K) {
+            cout << "0\n";
+        } else {
+            cout << dividers[dividers.size() - (K - cnt)] << "\n";
+        }
     }
     return 0;
 }
