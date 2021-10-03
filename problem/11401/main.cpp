82e013e9557abd6d05f907fbbd7e96617e546c81
Chayoung You <yousbe@gmail.com>
2021-10-03 19:21:34 +0900

    11401: reduce time, memory by removing variables

--- problem/11401/main.cpp
+++ problem/11401/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 const int MOD = static_cast<int>(1e9) + 7;
 int N, K;
-vector<int> fact;
 
 int mod_pow(int n, int k) {
     int res = 1;
@@ -27,15 +26,19 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N >> K;
-    fact.resize(N + 1);
-    int num = 1;
-    fact[0] = num;
+    int fact_n = 1;
+    int fact_k = 1;
     for (int i = 1; i <= N; i++) {
-        num = 1LL * num * i % MOD;
-        fact[i] = num;
+        fact_n = 1LL * fact_n * i % MOD;
+        if (i == K) {
+            fact_k = 1LL * fact_k * fact_n % MOD;
+        }
+        if (i == N - K) {
+            fact_k = 1LL * fact_k * fact_n % MOD;
+        }
     }
-    int ans = fact[N];
-    ans = 1LL * ans * mod_pow(1LL * fact[K] * fact[N - K] % MOD, MOD - 2) % MOD;
+    int ans = fact_n;
+    ans = 1LL * ans * mod_pow(fact_k, MOD - 2) % MOD;
     cout << ans << "\n";
     return 0;
 }
