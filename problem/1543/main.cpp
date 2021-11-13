cf08ad9918374055c529b36399fe691b7b4482f4
Chayoung You <yousbe@gmail.com>
2021-11-13 15:01:31 +0900

    1543: reduce time, memory by reducing checks

--- problem/1543/main.cpp
+++ problem/1543/main.cpp
@@ -8,17 +8,12 @@ string doc;
 string word;
 int doc_sz;
 int word_sz;
-vector<int> cache;
 
 int solve(int idx) {
     if (doc_sz - idx < word_sz) {
         return 0;
     }
-    int& ans = cache[idx];
-    if (ans >= 0) {
-        return ans;
-    }
-    ans = 0;
+    int ans = 0;
     int i = idx;
     while (doc_sz - i >= word_sz) {
         if (doc[i] == word[0]) {
@@ -27,7 +22,7 @@ int solve(int idx) {
                 offset++;
             }
             if (offset == word_sz) {
-                ans = max(ans, 1 + solve(i + offset));
+                return 1 + solve(i + offset);
             }
         }
         i++;
@@ -42,7 +37,6 @@ int main() {
     getline(cin, word);
     doc_sz = doc.size();
     word_sz = word.size();
-    cache.resize(doc_sz, -1);
     cout << solve(0) << "\n";
     return 0;
 }
