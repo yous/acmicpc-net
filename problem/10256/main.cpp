193c1ff38d9d0c82a4c2be94dda4653d4721feaa
Chayoung You <yousbe@gmail.com>
2021-11-27 18:58:06 +0900

    10256: solve in C++17

---
+++ problem/10256/main.cpp
@@ -0,0 +1,139 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int marker_sz, lo, hi;
+
+int acgt_idx(char c) {
+    switch (c) {
+        case 'A':
+            return 0;
+        case 'C':
+            return 1;
+        case 'G':
+            return 2;
+        case 'T':
+            return 3;
+    }
+    return -1;
+}
+
+struct Trie {
+    vector<Trie*> nodes = vector<Trie*>(4);
+    Trie* fail_node = nullptr;
+    bool end = false;
+
+    ~Trie() {
+        for (int i = 0; i < 4; i++) {
+            if (nodes[i] != nullptr) {
+                delete nodes[i];
+            }
+        }
+    }
+
+    void insert(const string& s, int idx) {
+        if (idx == marker_sz) {
+            end = true;
+            return;
+        }
+        Trie*& child = nodes[acgt_idx(s[idx])];
+        if (child == nullptr) {
+            child = new Trie;
+        }
+        int next_idx;
+        if (idx < lo - 1) {
+            next_idx = idx + 1;
+        } else if (idx == lo - 1) {
+            next_idx = hi - 1;
+        } else if (idx == lo) {
+            next_idx = hi;
+        } else if (idx < hi) {
+            next_idx = idx - 1;
+        } else {
+            next_idx = idx + 1;
+        }
+        return child->insert(s, next_idx);
+    }
+
+    int find(string::const_iterator it, const string::const_iterator send) {
+        int ans = 0;
+        if (it == send) {
+            if (end) {
+                ans++;
+            }
+            return ans;
+        }
+        if (end) {
+            ans++;
+        }
+        int idx = acgt_idx(*it);
+        Trie*& child = nodes[idx];
+        if (child == nullptr) {
+            return ans + find_fail_node(idx)->find(next(it), send);
+        }
+        return ans + child->find(next(it), send);
+    }
+
+    Trie* find_fail_node(int idx) {
+        Trie* tmp = fail_node;
+        while (tmp->fail_node != tmp && tmp->nodes[idx] == nullptr) {
+            tmp = tmp->fail_node;
+        }
+        if (tmp->nodes[idx] != nullptr) {
+            tmp = tmp->nodes[idx];
+        }
+        return tmp;
+    }
+
+    static void build_failure_func(Trie* root) {
+        queue<Trie*> qu;
+        root->fail_node = root;
+        for (int i = 0; i < 4; i++) {
+            if (root->nodes[i] == nullptr) {
+                continue;
+            }
+            root->nodes[i]->fail_node = root;
+            qu.emplace(root->nodes[i]);
+        }
+        while (!qu.empty()) {
+            Trie* node = qu.front();
+            qu.pop();
+            for (int i = 0; i < 4; i++) {
+                if (node->nodes[i] == nullptr) {
+                    continue;
+                }
+                node->nodes[i]->fail_node = node->find_fail_node(i);
+                qu.emplace(node->nodes[i]);
+            }
+            node->end |= node->fail_node->end;
+        }
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin >> T;
+    while (T-- > 0) {
+        int N, M;
+        cin >> N >> M;
+        Trie trie;
+        string dna, marker;
+        cin >> dna >> marker;
+        marker_sz = marker.size();
+        lo = hi = marker_sz + 1;
+        trie.insert(marker, 0);
+        for (lo = 0; lo < marker_sz - 1; lo++) {
+            for (hi = lo + 2; hi <= marker_sz; hi++) {
+                trie.insert(marker, (lo == 0 ? hi - 1 : 0));
+            }
+        }
+        Trie::build_failure_func(&trie);
+        cout << trie.find(dna.begin(), dna.end()) << "\n";
+    }
+    return 0;
+}
