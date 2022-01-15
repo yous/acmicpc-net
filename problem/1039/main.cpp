17c85a1af5ecdb36a214d832f6efefc023b5df5a
Chayoung You <yousbe@gmail.com>
2022-01-15 18:21:42 +0900

    1039: reduce time, memory by using queue

--- problem/1039/main.cpp
+++ problem/1039/main.cpp
@@ -1,37 +1,9 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
 
 using namespace std;
-
-int N_sz;
-vector<vector<int>> cache;
-
-int solve(string& n, int k) {
-    if (k == 0) {
-        return stoi(n);
-    }
-    int& ans = cache[stoi(n)][k - 1];
-    if (ans > 0) {
-        return ans;
-    }
-    for (int j = 1; j < N_sz; j++) {
-        if (n[j] == '0') {
-            continue;
-        }
-        swap(n[0], n[j]);
-        ans = max(ans, solve(n, k - 1));
-        swap(n[0], n[j]);
-    }
-    for (int i = 1; i < N_sz - 1; i++) {
-        for (int j = i + 1; j < N_sz; j++) {
-            swap(n[i], n[j]);
-            ans = max(ans, solve(n, k - 1));
-            swap(n[i], n[j]);
-        }
-    }
-    return ans;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -42,17 +14,53 @@ int main() {
         cout << "-1\n";
         return 0;
     }
-    if (N == 1000000) {
-        cout << N << "\n";
-        return 0;
-    }
     string N_str = to_string(N);
-    N_sz = N_str.size();
-    int sz = 1;
-    for (int i = 0; i < N_sz; i++) {
-        sz *= 10;
+    int N_sz = N_str.size();
+    sort(N_str.rbegin(), N_str.rend());
+    vector<bool> in_qu(stoi(N_str) + 1);
+    queue<int> qu;
+    in_qu[N] = true;
+    qu.emplace(N);
+    for (int k = 0; k < K; k++) {
+        int sz = qu.size();
+        while (sz-- > 0) {
+            int n = qu.front();
+            qu.pop();
+            in_qu[n] = false;
+            string n_str = to_string(n);
+            for (int j = 1; j < N_sz; j++) {
+                if (n_str[j] == '0') {
+                    continue;
+                }
+                swap(n_str[0], n_str[j]);
+                int new_n = stoi(n_str);
+                swap(n_str[0], n_str[j]);
+                if (!in_qu[new_n]) {
+                    in_qu[new_n] = true;
+                    qu.emplace(new_n);
+                }
+            }
+            for (int i = 1; i < N_sz - 1; i++) {
+                for (int j = i + 1; j < N_sz; j++) {
+                    swap(n_str[i], n_str[j]);
+                    int new_n = stoi(n_str);
+                    swap(n_str[i], n_str[j]);
+                    if (!in_qu[new_n]) {
+                        in_qu[new_n] = true;
+                        qu.emplace(new_n);
+                    }
+                }
+            }
+        }
     }
-    cache.resize(sz + 1, vector<int>(K));
-    cout << solve(N_str, K) << "\n";
+    int ans = 0;
+    while (!qu.empty()) {
+        int num = qu.front();
+        qu.pop();
+        if (ans < num) {
+            ans = num;
+        }
+    }
+    cout << ans << "\n";
     return 0;
 }
