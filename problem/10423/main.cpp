86de6b18eae7366df64a78a905025f596d21a052
Chayoung You <yousbe@gmail.com>
2022-01-21 02:36:30 +0900

    10423: reduce memory by removing variables

--- problem/10423/main.cpp
+++ problem/10423/main.cpp
@@ -5,34 +5,6 @@
 
 using namespace std;
 
-struct DisjointSet {
-    short N;
-    vector<short> group;
-
-    DisjointSet(short N) : N(N), group(N) {
-        for (int i = 0; i < N; i++) {
-            group[i] = i;
-        }
-    }
-
-    short find(short n) {
-        if (n == group[n]) {
-            return n;
-        }
-        return group[n] = find(group[n]);
-    }
-
-    bool merge(short p, short q) {
-        short gp = find(p);
-        short gq = find(q);
-        if (gp != gq) {
-            group[gq] = gp;
-            return true;
-        }
-        return false;
-    }
-};
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -40,11 +12,11 @@ int main() {
     int M;
     short K;
     cin >> N >> M >> K;
-    vector<bool> station(N);
+    vector<bool> visited(N);
     for (int i = 0; i < K; i++) {
         short num;
         cin >> num;
-        station[num - 1] = true;
+        visited[num - 1] = true;
     }
     vector<vector<pair<short, short>>> graph(N);
     for (int i = 0; i < M; i++) {
@@ -53,10 +25,9 @@ int main() {
         graph[u - 1].emplace_back(v - 1, w);
         graph[v - 1].emplace_back(u - 1, w);
     }
-    DisjointSet ds(N);
     priority_queue<tuple<short, short, short>> pq;
     for (int u = 0; u < N; u++) {
-        if (station[u]) {
+        if (visited[u]) {
             for (auto [v, w] : graph[u]) {
                 pq.emplace(-w, u, v);
             }
@@ -67,11 +38,11 @@ int main() {
         auto [w, u, v] = pq.top();
         w = -w;
         pq.pop();
-        if (station[ds.find(v)]) {
+        if (visited[v]) {
             continue;
         }
+        visited[v] = true;
         ans += w;
-        ds.merge(u, v);
         for (auto [v2, w2] : graph[v]) {
             pq.emplace(-w2, v, v2);
         }
