3580b52d4f0019d1e9f1d8fa6a6b6c59037c4489
Chayoung You <yousbe@gmail.com>
2022-01-21 03:47:33 +0900

    1994: solve in C++17

---
+++ problem/1994/main.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> nums;
+vector<vector<int>> cache;
+
+int solve(int idx, int prev_idx) {
+    if (idx >= N) {
+        return 0;
+    }
+    int& ans = cache[idx][prev_idx];
+    if (ans > 0) {
+        return ans;
+    }
+    ans = 1;
+    int diff = nums[idx] - nums[prev_idx];
+    int next_idx = lower_bound(next(nums.begin(), idx + 1), nums.end(), nums[idx] + diff) - nums.begin();
+    if (next_idx < N && nums[next_idx] == nums[idx] + diff) {
+        ans = 1 + solve(next_idx, idx);
+    }
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N;
+    nums.resize(N);
+    for (int& num : nums) {
+        cin >> num;
+    }
+    if (N == 1) {
+        cout << "1\n";
+        return 0;
+    }
+    cache.resize(N, vector<int>(N));
+    sort(nums.begin(), nums.end());
+    int ans = 2;
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            ans = max(ans, 1 + solve(j, i));
+        }
+    }
+    cout << ans << "\n";
+    return 0;
+}
