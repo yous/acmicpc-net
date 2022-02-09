01fc127b9d1d6a8c531342a2f4127b06cbe4a308
Chayoung You <yousbe@gmail.com>
2022-02-10 03:19:48 +0900

    20943: reduce time, memory by sorting

--- problem/20943/main.cpp
+++ problem/20943/main.cpp
@@ -1,7 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
-#include <numeric>
 #include <vector>
 
 using namespace std;
@@ -11,27 +9,33 @@ int main() {
     cin.tie(nullptr);
     int N;
     cin >> N;
-    map<pair<int, int>, int> ma;
-    for (int i = 0; i < N; i++) {
-        int a, b, c;
+    vector<pair<int, int>> slopes(N);
+    for (auto& [a, b] : slopes) {
+        int c;
         cin >> a >> b >> c;
         if (a < 0) {
             a = -a;
             b = -b;
         }
-        if (a == 0) {
-            b = 1;
-        } else if (b == 0) {
-            a = 1;
-        }
-        int g = gcd(a, abs(b));
-        ma[make_pair(a / g, b / g)]++;
     }
+    sort(slopes.begin(), slopes.end(), [](const pair<int, int>& lhs, const pair<int, int>& rhs) {
+        return 1LL * lhs.first * rhs.second < 1LL * rhs.first * lhs.second;
+    });
     long long ans = 1LL * N * (N - 1) / 2;
-    for (auto [k, v] : ma) {
-        if (v > 1) {
-            ans -= 1LL * v * (v - 1) / 2;
+    pair<int, int>* prev_slope = &slopes[0];
+    int cnt = 1;
+    for (int i = 1; i < N; i++) {
+        auto& [a, b] = slopes[i];
+        if (1LL * prev_slope->first * b == 1LL * a * prev_slope->second) {
+            cnt++;
+        } else {
+            ans -= 1LL * cnt * (cnt - 1) / 2;
+            cnt = 1;
         }
+        prev_slope = &slopes[i];
+    }
+    if (cnt > 1) {
+        ans -= 1LL * cnt * (cnt - 1) / 2;
     }
     cout << ans << "\n";
     return 0;
