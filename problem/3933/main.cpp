8fa8ec79f389b632799b9032f1e6fb48f37c047f
Chayoung You <yousbe@gmail.com>
2022-02-21 04:47:20 +0900

    3933: reduce time, memory by reducing variables

--- problem/3933/main.cpp
+++ problem/3933/main.cpp
@@ -7,16 +7,13 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    vector<vector<int>> cache(182, vector<int>((1 << 15) + 1));
+    vector<vector<int>> cache(5, vector<int>((1 << 15) + 1));
+    cache[0][0] = 1;
     for (int i = 1; i * i <= (1 << 15); i++) {
         int i_sq = i * i;
-        cache[i][i_sq]++;
-        for (int j = i; i_sq + j * j <= (1 << 15); j++) {
-            int j_sq = j * j;
-            cache[i][i_sq + j_sq]++;
-            for (int k = j; i_sq + j_sq + k * k <= (1 << 15); k++) {
-                int k_sq = k * k;
-                cache[i][i_sq + j_sq + k_sq]++;
+        for (int cnt = 1; cnt <= 4; cnt++) {
+            for (int j = 0; j + i_sq <= (1 << 15) + 1; j++) {
+                cache[cnt][j + i_sq] += cache[cnt - 1][j];
             }
         }
     }
@@ -26,15 +23,8 @@ int main() {
             break;
         }
         int ans = 0;
-        for (int i = 1; i * i <= num; i++) {
-            int rem = num - i * i;
-            if (rem == 0) {
-                ans++;
-            } else {
-                for (int j = i; j * j <= rem; j++) {
-                    ans += cache[j][num - i * i];
-                }
-            }
+        for (int cnt = 1; cnt <= 4; cnt++) {
+            ans += cache[cnt][num];
         }
         cout << ans << "\n";
     }
