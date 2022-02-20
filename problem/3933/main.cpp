3166dfe8fb159f8fcc1f80b059d7609f227a97a5
Chayoung You <yousbe@gmail.com>
2022-02-21 04:30:25 +0900

    3933: reduce time, memory by preparing data

--- problem/3933/main.cpp
+++ problem/3933/main.cpp
@@ -4,35 +4,39 @@
 
 using namespace std;
 
-vector<vector<vector<int>>> cache(4, vector<vector<int>>(182, vector<int>((1 << 15) + 1, -1)));
-
-int solve(int cnt, int min_num, int num) {
-    if (cnt == -1) {
-        return (num == 0);
-    }
-    if (num == 0) {
-        return 1;
-    }
-    int& ans = cache[cnt][min_num][num];
-    if (ans >= 0) {
-        return ans;
-    }
-    ans = 0;
-    for (int i = min_num; i * i <= num; i++) {
-        ans += solve(cnt - 1, i, num - i * i);
-    }
-    return ans;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    vector<vector<int>> cache(182, vector<int>((1 << 15) + 1));
+    for (int i = 1; i * i <= (1 << 15); i++) {
+        int i_sq = i * i;
+        cache[i][i_sq]++;
+        for (int j = i; i_sq + j * j <= (1 << 15); j++) {
+            int j_sq = j * j;
+            cache[i][i_sq + j_sq]++;
+            for (int k = j; i_sq + j_sq + k * k <= (1 << 15); k++) {
+                int k_sq = k * k;
+                cache[i][i_sq + j_sq + k_sq]++;
+            }
+        }
+    }
     int num;
     while (cin >> num) {
         if (num == 0) {
             break;
         }
-        cout << solve(3, 1, num) << "\n";
+        int ans = 0;
+        for (int i = 1; i * i <= num; i++) {
+            int rem = num - i * i;
+            if (rem == 0) {
+                ans++;
+            } else {
+                for (int j = i; j * j <= rem; j++) {
+                    ans += cache[j][num - i * i];
+                }
+            }
+        }
+        cout << ans << "\n";
     }
     return 0;
 }
