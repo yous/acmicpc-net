ea28d398d4591cd0e4b16910481166ccbd2cd827
Chayoung You <yousbe@gmail.com>
2022-02-22 03:41:51 +0900

    2108: fix for edge case, reduce time, memory by using vector

--- problem/2108/main.cpp
+++ problem/2108/main.cpp
@@ -1,29 +1,24 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <set>
 #include <vector>
 
 using namespace std;
 
-int N;
-vector<int> nums;
-int counts[8001];
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    int N;
     cin >> N;
-    int i;
+    vector<int> nums(N);
+    vector<int> counts(8001);
     int sum = 0;
     int freq = 0;
-    set<int> freq_nums;
+    vector<int> freq_nums;
     int min_num = 4000;
     int max_num = -4000;
-    for (i = 0; i < N; i++) {
-        int num;
+    for (int& num : nums) {
         cin >> num;
-        nums.push_back(num);
         sum += num;
         if (num < min_num) {
             min_num = num;
@@ -34,19 +29,20 @@ int main() {
         counts[num + 4000]++;
         if (counts[num + 4000] > freq) {
             freq_nums.clear();
-            freq_nums.insert(num);
+            freq_nums.emplace_back(num);
             freq = counts[num + 4000];
         } else if (counts[num + 4000] == freq) {
-            freq_nums.insert(num);
+            freq_nums.emplace_back(num);
         }
     }
     sort(nums.begin(), nums.end());
-    cout << round(sum / static_cast<double>(N)) << "\n";
+    cout << int(round(sum / double(N))) << "\n";
     cout << nums[N / 2] << "\n";
     if (freq_nums.size() > 1) {
-        cout << *next(freq_nums.begin()) << "\n";
+        sort(freq_nums.begin(), freq_nums.end());
+        cout << freq_nums[1] << "\n";
     } else {
-        cout << *freq_nums.begin() << "\n";
+        cout << freq_nums[0] << "\n";
     }
     cout << max_num - min_num << "\n";
     return 0;
