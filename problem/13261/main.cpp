c7dff0c5c5ee714aaaf7191fe46a2784409db5b5
Chayoung You <yousbe@gmail.com>
2022-02-26 17:06:58 +0900

    13261: reduce time, memory by removing variables

--- problem/13261/main.cpp
+++ problem/13261/main.cpp
@@ -6,24 +6,25 @@ using namespace std;
 
 const long long INF = 4e18;
 vector<long long> prefix_sum;
-vector<vector<long long>> cache;
+vector<long long> cur_cache;
+vector<long long> prev_cache;
 
 long long cost(int l, int r) {
     return (prefix_sum[r + 1] - prefix_sum[l]) * (r - l + 1);
 }
 
-void solve(int cnt, int l, int r, int optl, int optr) {
+void solve(int l, int r, int optl, int optr) {
     if (l > r) {
         return;
     }
     int mid = (l + r) / 2;
     pair<long long, int> opt = {INF, -1};
     for (int i = optl, ed = min(optr, mid); i <= ed; i++) {
-        opt = min(opt, {(i > 0 ? cache[cnt - 1][i - 1] : 0) + cost(i, mid), i});
+        opt = min(opt, {(i > 0 ? prev_cache[i - 1] : 0) + cost(i, mid), i});
     }
-    cache[cnt][mid] = opt.first;
-    solve(cnt, l, mid - 1, optl, opt.second);
-    solve(cnt, mid + 1, r, opt.second, optr);
+    cur_cache[mid] = opt.first;
+    solve(l, mid - 1, optl, opt.second);
+    solve(mid + 1, r, opt.second, optr);
 }
 
 int main() {
@@ -39,13 +40,15 @@ int main() {
         sum += C[i];
         prefix_sum[i + 1] = sum;
     }
-    cache.resize(G, vector<long long>(L));
+    cur_cache.resize(L);
+    prev_cache.resize(L);
     for (int i = 0; i < L; i++) {
-        cache[0][i] = cost(0, i);
+        prev_cache[i] = cost(0, i);
     }
     for (int i = 1; i < G; i++) {
-        solve(i, 0, L - 1, 0, L - 1);
+        solve(0, L - 1, 0, L - 1);
+        swap(cur_cache, prev_cache);
     }
-    cout << cache[G - 1][L - 1] << "\n";
+    cout << prev_cache[L - 1] << "\n";
     return 0;
 }
