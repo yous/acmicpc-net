05482eaa0e10e05aba96ff444cf7feb07f5d4f2a
Chayoung You <yousbe@gmail.com>
2022-03-09 01:27:54 +0900

    1394: reduce time by reducing loop

--- problem/1394/main.cpp
+++ problem/1394/main.cpp
@@ -22,13 +22,12 @@ int main() {
     int pw_sz = password.size();
     vector<int> power(pw_sz);
     int p = 1;
-    for (int i = 0; i < pw_sz; i++) {
-        power[i] = p;
-        p = p * sz % MOD;
-    }
+    power[0] = p;
     int ans = 0;
-    for (int i = 0; i < pw_sz - 1; i++) {
-        ans = (ans + power[i + 1]) % MOD;
+    for (int i = 1; i < pw_sz; i++) {
+        p = p * sz % MOD;
+        power[i] = p;
+        ans = (ans + power[i]) % MOD;
     }
     for (int i = 0; i < pw_sz; i++) {
         ans = (ans + power[pw_sz - i - 1] * char_map[password[i]]) % MOD;
