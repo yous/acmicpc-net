4c6e6d081cb76fb67b3be8accd174d9f22be9934
Chayoung You <yousbe@gmail.com>
2022-03-26 04:59:47 +0900

    21757: solve in C++17

---
+++ problem/21757/main.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin >> N;
+    vector<int> prefix_sum(N);
+    int sum = 0;
+    for (int& num : prefix_sum) {
+        cin >> num;
+        sum += num;
+        num = sum;
+    }
+    int target = sum / 4;
+    long long cnt[3] = {0};
+    for (int i = 0; i < N - 1; i++) {
+        if (prefix_sum[i] == target * 3) {
+            cnt[2] += cnt[1];
+        }
+        if (prefix_sum[i] == target * 2) {
+            cnt[1] += cnt[0];
+        }
+        if (prefix_sum[i] == target) {
+            cnt[0]++;
+        }
+    }
+    cout << cnt[2] << "\n";
+    return 0;
+}
