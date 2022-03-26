0b3954b24ed8d141512f33f044067d6d8a2ec6ff
Chayoung You <yousbe@gmail.com>
2022-03-26 15:22:25 +0900

    24724: solve in C++17

---
+++ problem/24724/main.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    short T;
+    cin >> T;
+    for (short t = 1; t <= T; t++) {
+        short N;
+        cin >> N;
+        int A, B;
+        cin >> A >> B;
+        for (int i = 0; i < N; i++) {
+            int u, v;
+            cin >> u >> v;
+        }
+        cout << "Material Management " << t << "\n";
+        cout << "Classification ---- End!\n";
+    }
+    return 0;
+}
