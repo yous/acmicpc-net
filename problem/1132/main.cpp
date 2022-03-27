15e9493dc93de93e11f8030fb9f75d6cf73f41e1
Chayoung You <yousbe@gmail.com>
2022-03-28 01:34:39 +0900

    1132: reduce time by sorting

--- problem/1132/main.cpp
+++ problem/1132/main.cpp
@@ -4,36 +4,12 @@
 
 using namespace std;
 
-long long solve(int num, vector<vector<short>>& cnt, vector<short>& digit, vector<bool>& used) {
-    if (num == 10) {
-        long long res = 0;
-        for (int i = 0; i < 12; i++) {
-            res = res * 10;
-            for (int j = 0; j < 10; j++) {
-                res += cnt[i][j] * digit[j];
-            }
-        }
-        return res;
-    }
-    long long res = 0;
-    for (int i = 0; i <= 9; i++) {
-        if (used[i]) {
-            continue;
-        }
-        digit[i] = num;
-        used[i] = true;
-        res = max(res, solve(num + 1, cnt, digit, used));
-        used[i] = false;
-    }
-    return res;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     short N;
     cin >> N;
-    vector<vector<short>> cnt(12, vector<short>(10));
+    vector<vector<short>> cnt(10, vector<short>(12));
     vector<bool> not_zero(10);
     for (int i = 0; i < N; i++) {
         string s;
@@ -41,20 +17,30 @@ int main() {
         not_zero[s[0] - 'A'] = true;
         int sz = s.size();
         for (int j = 0; j < sz; j++) {
-            cnt[12 - sz + j][s[j] - 'A']++;
+            cnt[s[j] - 'A'][12 - sz + j]++;
         }
     }
-    vector<bool> used(10);
-    vector<short> digit(10);
-    long long ans = 0;
+    vector<pair<long long, int>> alpha(10);
     for (int i = 0; i <= 9; i++) {
-        if (not_zero[i]) {
+        auto& p = alpha[i];
+        p = {0, i};
+        for (int j = 0; j < 12; j++) {
+            p.first = p.first * 10 + cnt[i][j];
+        }
+    }
+    sort(alpha.begin(), alpha.end());
+    int zero_idx = 0;
+    while (not_zero[alpha[zero_idx].second]) {
+        zero_idx++;
+    }
+    long long ans = 0;
+    int digit = 1;
+    for (int i = 0; i <= 9; i++) {
+        if (i == zero_idx) {
             continue;
         }
-        digit[i] = 0;
-        used[i] = true;
-        ans = max(ans, solve(1, cnt, digit, used));
-        used[i] = false;
+        ans += alpha[i].first * digit;
+        digit++;
     }
     cout << ans << "\n";
     return 0;
