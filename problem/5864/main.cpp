de9063ebd8445cbc54e05ceb654c9522267a8918
Chayoung You <yousbe@gmail.com>
2022-04-19 04:47:45 +0900

    5864: reduce memory by reducing function arguments

--- problem/5864/main.cpp
+++ problem/5864/main.cpp
@@ -5,8 +5,10 @@
 using namespace std;
 
 short N, A, B;
+vector<int> cows;
+vector<unsigned int> cache;
 
-unsigned int solve(short idx, vector<int>& cows, vector<unsigned int>& cache) {
+unsigned int solve(short idx) {
     if (idx >= N) {
         return 0;
     }
@@ -15,9 +17,9 @@ unsigned int solve(short idx, vector<int>& cows, vector<unsigned int>& cache) {
         return ans;
     }
     int num = cows[idx];
-    ans = A * 2 + solve(idx + 1, cows, cache);
+    ans = A * 2 + solve(idx + 1);
     for (int i = idx + 1; i < N; i++) {
-        ans = min(ans, A * 2 + B * (cows[i] - num) + solve(i + 1, cows, cache));
+        ans = min(ans, A * 2 + B * (cows[i] - num) + solve(i + 1));
     }
     return ans;
 }
@@ -26,13 +28,13 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N >> A >> B;
-    vector<int> cows(N);
+    cows.resize(N);
     for (int& num : cows) {
         cin >> num;
     }
     sort(cows.begin(), cows.end());
-    vector<unsigned int> cache(N, 0xFFFFFFFF);
-    unsigned int ans = solve(0, cows, cache);
+    cache.resize(N, 0xFFFFFFFF);
+    unsigned int ans = solve(0);
     cout << ans / 2 << (ans % 2 == 1 ? ".5" : "") << "\n";
     return 0;
 }
