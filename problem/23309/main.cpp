2ce498322c78812b6dc7361519bb601bd847973e
Chayoung You <yousbe@gmail.com>
2022-04-21 02:09:15 +0900

    23309: reduce time, memory by using int pair

--- problem/23309/main.cpp
+++ problem/23309/main.cpp
@@ -4,47 +4,24 @@
 
 using namespace std;
 
-struct Node {
-    int num;
-    Node* prev;
-    Node* next;
-
-    Node(int num) : num(num), prev(this), next(this) {}
-};
-
-void insert(Node* node, Node* item) {
-    item->next = node->next;
-    item->prev = node;
-    node->next->prev = item;
-    node->next = item;
-}
-
-void erase(Node* node) {
-    node->prev->next = node->next;
-    node->next->prev = node->prev;
-    delete node;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int N, M;
     cin >> N >> M;
-    vector<Node*> unique_map(1000001);
+    vector<pair<int, int>> unique_map(1000001);
     int first_num;
     cin >> first_num;
-    unique_map[first_num] = new Node(first_num);
     int prev_num = first_num;
     for (int i = 1; i < N; i++) {
         int num;
         cin >> num;
-        unique_map[num] = new Node(num);
-        unique_map[num]->prev = unique_map[prev_num];
-        unique_map[prev_num]->next = unique_map[num];
+        unique_map[num].first = prev_num;
+        unique_map[prev_num].second = num;
         prev_num = num;
     }
-    unique_map[prev_num]->next = unique_map[first_num];
-    unique_map[first_num]->prev = unique_map[prev_num];
+    unique_map[prev_num].second = first_num;
+    unique_map[first_num].first = prev_num;
     for (int m = 0; m < M; m++) {
         char cmd1, cmd2;
         cin >> cmd1 >> cmd2;
@@ -52,25 +29,33 @@ int main() {
             int i, j;
             cin >> i >> j;
             if (cmd2 == 'N') {
-                int i_next = unique_map[i]->next->num;
+                int i_next = unique_map[i].second;
                 cout << i_next << "\n";
-                unique_map[j] = new Node(j);
-                insert(unique_map[i], unique_map[j]);
+                unique_map[j].first = i;
+                unique_map[j].second = i_next;
+                unique_map[i_next].first = j;
+                unique_map[i].second = j;
             } else {
-                int i_prev = unique_map[i]->prev->num;
+                int i_prev = unique_map[i].first;
                 cout << i_prev << "\n";
-                unique_map[j] = new Node(j);
-                insert(unique_map[i]->prev, unique_map[j]);
+                unique_map[j].first = i_prev;
+                unique_map[j].second = i;
+                unique_map[i_prev].second = j;
+                unique_map[i].first = j;
             }
         } else {
             int i;
             cin >> i;
             if (cmd2 == 'N') {
-                cout << unique_map[i]->next->num << "\n";
-                erase(unique_map[i]->next);
+                int i_next = unique_map[i].second;
+                cout << i_next << "\n";
+                unique_map[unique_map[i_next].second].first = i;
+                unique_map[i].second = unique_map[i_next].second;
             } else {
-                cout << unique_map[i]->prev->num << "\n";
-                erase(unique_map[i]->prev);
+                int i_prev = unique_map[i].first;
+                cout << i_prev << "\n";
+                unique_map[i].first = unique_map[i_prev].first;
+                unique_map[unique_map[i_prev].first].second = i;
             }
         }
     }
