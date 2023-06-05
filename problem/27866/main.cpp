e7783e65934b9f8fa036bbdf18755d1a3cf99ccc
Chayoung You <yousbe@gmail.com>
2023-06-06 00:33:04 +0900

    27866: solve in C++17

---
+++ problem/27866/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string S;
+    cin >> S;
+    short i;
+    cin >> i;
+    cout << S[i - 1] << "\n";
+    return 0;
+}
