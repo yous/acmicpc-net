9aaf5d3bdb53a20f7b795e77a261cc2989406d0c
Chayoung You <yousbe@gmail.com>
2023-06-06 00:31:28 +0900

    25083: solve in C++17

---
+++ problem/25083/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << "         ,r'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+    return 0;
+}
