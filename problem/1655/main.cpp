72622aaa2e685dac2d65894b1e20c8d2ff0647fd
Chayoung You <yousbe@gmail.com>
2023-11-29 16:18:20 +0900

    1655: reduce code

--- problem/1655/main.cpp
+++ problem/1655/main.cpp
@@ -12,46 +12,17 @@ int main() {
     cin >> N;
     priority_queue<short> pq_lower;
     priority_queue<short> pq_upper;
-    int lower_cnt = 0;
-    int upper_cnt = 0;
     for (int i = 0; i < N; i++) {
         short num;
         cin >> num;
-        if (lower_cnt == 0) {
-            pq_lower.emplace(num);
-            lower_cnt++;
-        } else if (upper_cnt == 0) {
-            short lower = pq_lower.top();
-            if (num >= lower) {
-                pq_upper.emplace(-num);
-            } else {
-                pq_lower.pop();
-                pq_upper.emplace(-lower);
-                pq_lower.emplace(num);
-            }
-            upper_cnt++;
+        if (i % 2 == 0) {
+            pq_upper.emplace(-num);
+            pq_lower.emplace(-pq_upper.top());
+            pq_upper.pop();
         } else {
-            short lower = pq_lower.top();
-            short upper = -pq_upper.top();
-            if (lower_cnt == upper_cnt) {
-                if (num <= upper) {
-                    pq_lower.emplace(num);
-                } else {
-                    pq_upper.pop();
-                    pq_lower.emplace(upper);
-                    pq_upper.emplace(-num);
-                }
-                lower_cnt++;
-            } else {
-                if (num >= lower) {
-                    pq_upper.emplace(-num);
-                } else {
-                    pq_lower.pop();
-                    pq_upper.emplace(-lower);
-                    pq_lower.emplace(num);
-                }
-                upper_cnt++;
-            }
+            pq_lower.emplace(num);
+            pq_upper.emplace(-pq_lower.top());
+            pq_lower.pop();
         }
         cout << pq_lower.top() << "\n";
     }
