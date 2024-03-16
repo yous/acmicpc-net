61200707ee546124950e3c197d1ad5e8ee2f2bfa
Chayoung You <yousbe@gmail.com>
2024-03-16 14:54:29 +0900

    14940: reduce time and memory by using short

--- problem/14940/main.cpp
+++ problem/14940/main.cpp
@@ -5,55 +5,54 @@
 
 using namespace std;
 
-const short dy[] = {-1, 1, 0, 0};
-const short dx[] = {0, 0, -1, 1};
+const int dy[] = {-1, 1, 0, 0};
+const int dx[] = {0, 0, -1, 1};
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     short N, M;
     cin >> N >> M;
-    vector<vector<int>> field(N, vector<int>(M));
-    int sy, sx;
+    vector<vector<short>> field(N, vector<short>(M));
+    vector<vector<short>> dist(N, vector<short>(M, -1));
+    short dist_y = 0;
+    short dist_x = 0;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            int num;
-            cin >> num;
-            if (num == 2) {
-                sy = i;
-                sx = j;
-                field[i][j] = 0;
-            } else if (num == 1) {
-                field[i][j] = -1;
-            } else {
-                field[i][j] = num;
+            cin >> field[i][j];
+            if (field[i][j] == 2) {
+                dist_y = i;
+                dist_x = j;
+            } else if (field[i][j] == 0) {
+                dist[i][j] = 0;
             }
         }
     }
     queue<pair<short, short>> qu;
-    qu.emplace(sy, sx);
+    qu.emplace(dist_y, dist_x);
+    dist[dist_y][dist_x] = 0;
     int step = 1;
     while (!qu.empty()) {
         auto sz = qu.size();
         while (sz-- > 0) {
-            auto& [y, x] = qu.front();
+            auto [y, x] = qu.front();
             qu.pop();
             for (int i = 0; i < 4; i++) {
                 short ny = y + dy[i];
                 short nx = x + dx[i];
-                if (ny < 0 || ny >= N || nx < 0 || nx >= M || field[ny][nx] != -1) {
+                if (ny < 0 || ny >= N || nx < 0 || nx >= M || dist[ny][nx] >= 0) {
                     continue;
                 }
-                field[ny][nx] = step;
                 qu.emplace(ny, nx);
+                dist[ny][nx] = step;
             }
         }
         step++;
     }
-    for (auto& row : field) {
+    for (auto& row : dist) {
         cout << row[0];
-        for (int i = 1; i < M; i++) {
-            cout << " " << row[i];
+        for (int j = 1; j < M; j++) {
+            cout << " " << row[j];
         }
         cout << "\n";
     }
