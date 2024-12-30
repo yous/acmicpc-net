24d524d6232d874f917cca6a012d2ae2ad06ab74
Chayoung You <yousbe@gmail.com>
2024-12-31 02:57:10 +0900

    33040: solve in C++17

---
+++ problem/33040/main.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int H, W;
+    cin >> H >> W;
+    int C, D;
+    cin >> C >> D;
+    if (H > W + 1) {
+        cout << "-1\n";
+        return 0;
+    }
+    int min_sum = H * (H - 1) / 2;
+    if (C < min_sum || D < min_sum) {
+        cout << "-1\n";
+        return 0;
+    }
+    int D_rem = D;
+    for (int y = 0; y < H - 1; y++) {
+        int offset = (D_rem - min_sum) / (H - y);
+        bool first = true;
+        for (int x = 0; x < W - (offset + y); x++) {
+            if (first) {
+                first = false;
+            } else {
+                cout << " ";
+            }
+            cout << "1";
+        }
+        for (int x = 0; x < offset + y; x++) {
+            if (first) {
+                first = false;
+            } else {
+                cout << " ";
+            }
+            cout << "9";
+        }
+        D_rem -= offset + y;
+        min_sum -= y;
+        cout << "\n";
+    }
+    bool first = true;
+    for (int x = 0; x < W - D_rem; x++) {
+        if (first) {
+            first = false;
+        } else {
+            cout << " ";
+        }
+        cout << "1";
+    }
+    for (int x = 0; x < D_rem; x++) {
+        if (first) {
+            first = false;
+        } else {
+            cout << " ";
+        }
+        cout << "9";
+    }
+    cout << "\n";
+    return 0;
+}
