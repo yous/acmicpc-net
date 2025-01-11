09e3eae3ed55ee933158c9c3dce52afde5488c2d
Chayoung You <yousbe@gmail.com>
2025-01-11 16:49:42 +0900

    1311: reduce time, memory by reducing allocation

--- problem/1311/main.cpp
+++ problem/1311/main.cpp
@@ -6,28 +6,10 @@ using namespace std;
 
 const int INF = 1e9;
 
-short N;
-
-int solve(int idx, int visited, vector<vector<int>>& cache, vector<vector<short>>& D) {
-    if (idx == N) {
-        return 0;
-    }
-    auto& ans = cache[idx][visited];
-    if (ans != INF) {
-        return ans;
-    }
-    for (int i = 0; i < N; i++) {
-        if (visited & (1 << i)) {
-            continue;
-        }
-        ans = min(ans, D[idx][i] + solve(idx + 1, visited | (1 << i), cache, D));
-    }
-    return ans;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    short N;
     cin >> N;
     vector<vector<short>> D(N, vector<short>(N));
     for (auto& row : D) {
@@ -35,7 +17,16 @@ int main() {
             cin >> num;
         }
     }
-    vector<vector<int>> cache(N, vector<int>(1 << N, INF));
-    cout << solve(0, 0, cache, D) << "\n";
+    vector<int> cache(1 << N, INF);
+    cache[0] = 0;
+    for (int visited = 1; visited < (1 << N); visited++) {
+        int cnt = __builtin_popcount(visited);
+        for (int n = 0; n < N; n++) {
+            if (visited & (1 << n)) {
+                cache[visited] = min(cache[visited], D[cnt - 1][n] + cache[visited & ~(1 << n)]);
+            }
+        }
+    }
+    cout << cache[(1 << N) - 1] << "\n";
     return 0;
 }
