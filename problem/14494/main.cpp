c863a13dc76836101c5cb8261675c184772e8dc2
Chayoung You <yousbe@gmail.com>
2025-01-11 14:48:27 +0900

    14494: reduce time

--- problem/14494/main.cpp
+++ problem/14494/main.cpp
@@ -4,29 +4,26 @@
 
 using namespace std;
 
-const int MOD = static_cast<int>(1e9) + 7;
+const int MOD = int(1e9) + 7;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int N, M;
+    short N, M;
     cin >> N >> M;
-    vector<vector<int>> cache(N, vector<int>(M, 0));
-    cache[0][0] = 1;
-    for (int i = 0; i < N + M - 2; i++) {
-        for (int y = max(0, i - (M - 1)); y <= min(N - 1, i); y++) {
-            int x = i - y;
-            if (x < M - 1) {
-                cache[y][x + 1] = (cache[y][x + 1] + cache[y][x]) % MOD;
-                if (y < N - 1) {
-                    cache[y + 1][x + 1] = (cache[y + 1][x + 1] + cache[y][x]) % MOD;
-                }
-            }
-            if (y < N - 1) {
-                cache[y + 1][x] = (cache[y + 1][x] + cache[y][x]) % MOD;
-            }
+    vector<vector<int>> D(N, vector<int>(M));
+    D[0][0] = 1;
+    for (int y = 1; y < N; y++) {
+        D[y][0] = 1;
+    }
+    for (int x = 1; x < M; x++) {
+        D[0][x] = 1;
+    }
+    for (int y = 1; y < N; y++) {
+        for (int x = 1; x < M; x++) {
+            D[y][x] = ((D[y][x - 1] + D[y - 1][x]) % MOD + D[y - 1][x - 1]) % MOD;
         }
     }
-    cout << cache[N - 1][M - 1] << "\n";
+    cout << D[N - 1][M - 1] << "\n";
     return 0;
 }
